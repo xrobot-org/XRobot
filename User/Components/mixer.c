f6d8cc6a040d213ba98bf5060776fe865f36c46a
Qu Shen <503578404@qq.com>
2020-02-09 18:11:54 +0800

    完成主要Mixer实现。

--- User/Components/mixer.c
+++ User/Components/mixer.c
@@ -1,3 +1,46 @@
 /* 
 	
 */
+
+
+int Mixer_Mecanum(float vx, float vy, float wz, float *out, int len) {
+	if (len == 4) {
+		out[0] = -vx - vy + wz;
+		out[1] = vx - vy + wz;
+		out[2] = vx + vy + wz;
+		out[3] = -vx + vy + wz;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int Mixer_ParlFix4(float vx, float vy, float wz, float *out, int len) {
+	if (len == 4) {
+		out[0] = -vx;
+		out[1] = vx;
+		out[2] = vx;
+		out[3] = -vx;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int Mixer_ParlFix2(float vx, float vy, float wz, float *out, int len) {
+	if (len == 2) {
+		out[0] = -vx;
+		out[1] = vx;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int Mixer_OmniCross(float vx, float vy, float wz, float *out, int len) {
+	return 0;
+}
+
+int Mixer_OmniPlus(float vx, float vy, float wz, float *out, int len) {
+	return 0;
+}
--- User/Components/mixer.h
+++ User/Components/mixer.h
@@ -8,9 +8,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef int Mixer(float vx, float vy, float vz, float *lf, float *lr, float *rf, float *rr);
-
-int Mixer_Mecanum(float vx, float vy, float vz, float *lf, float *lr, float *rf, float *rr);
-int Mixer_ParlFix4(float vx, float vy, float vz, float *lf, float *lr, float *rf, float *rr);
-int Mixer_OmniCross(float vx, float vy, float vz, float *lf, float *lr, float *rf, float *rr);
-int Mixer_OmniPlus(float vx, float vy, float vz, float *lf, float *lr, float *rf, float *rr);
+int Mixer_Mecanum(float vx, float vy, float wz, float *out, int len);
+int Mixer_ParlFix4(float vx, float vy, float wz, float *out, int len);
+int Mixer_ParlFix2(float vx, float vy, float wz, float *out, int len);
+int Mixer_OmniCross(float vx, float vy, float wz, float *out, int len);
+int Mixer_OmniPlus(float vx, float vy, float wz, float *out, int len);
