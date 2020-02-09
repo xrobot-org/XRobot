900793b8e9d91a0dcdfabca79ac42855a81fa516
Qu Shen <503578404@qq.com>
2020-02-09 18:03:14 +0800

    创建命令解析任务。

--- User/Tasks/task_command.c
+++ User/Tasks/task_command.c
@@ -6,16 +6,18 @@
 /* Includes ------------------------------------------------------------------*/
 #include "task_common.h"
 
+/* Include 标准库*/
 /* Include Board相关的头文件 */
 /* Include Device相关的头文件。*/
 #include "dr16.h"
+
 /* Include Component相关的头文件。*/
 /* Include Module相关的头文件。*/
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-static const uint32_t delay_ms = 1000U / TASK_COMM_FREQ_HZ;
+static const uint32_t delay_ms = 1000u / TASK_COMMAND_FREQ_HZ;
 static int result = 0;
 static osStatus os_status = osOK;
 
@@ -26,21 +28,22 @@ DR16_t dr16;
 void Task_Comm(void const *argument) {
 	Task_Param_t *task_param = (Task_Param_t*)argument;
 	
-	/* 等待一段时间后再开始任务。*/
-	osDelay(TASK_COMM_INIT_DELAY);
+	/* Task Setup */
+	osDelay(TASK_COMMAND_INIT_DELAY);
 	
 	dr16.received_alert = task_param->thread.comm;
 	DR16_Init(&dr16);
 	
-	
-	
 	uint32_t previous_wake_time = osKernelSysTick();
 	while(1) {
-		/* 任务主体 */
+		/* Task */
 		result += DR16_StartReceiving(&dr16);
-		osSignalWait(DR16_SIGNAL_DATA_REDY, osWaitForever);
+		osSignalWait(DR16_SIGNAL_RAW_REDY, osWaitForever);
 		result += DR16_Parse(&dr16);
 		
+		osSignalSet(task_param->thread.ctrl_chassis, DR16_SIGNAL_DATA_REDY);
+		osSignalSet(task_param->thread.ctrl_gimbal, DR16_SIGNAL_DATA_REDY);
+		osSignalSet(task_param->thread.ctrl_shoot, DR16_SIGNAL_DATA_REDY);
 		
 		osDelayUntil(&previous_wake_time, delay_ms);
 	}
