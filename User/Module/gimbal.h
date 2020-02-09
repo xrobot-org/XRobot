79f6b58aa96d3b57059ee722602aebed9be27622
Qu Shen <503578404@qq.com>
2020-02-09 18:09:24 +0800

    Update gimbal.h

--- User/Module/gimbal.h
+++ User/Module/gimbal.h
@@ -1,12 +1,33 @@
 #pragma once
 
+
+/* Includes ------------------------------------------------------------------*/
+/* Include 标准库*/
+/* Include Board相关的头文件 */
+/* Include Device相关的头文件。*/
+/* Include Component相关的头文件。*/
+#include "pid.h"
+#include "ahrs.h"
+
+/* Include Module相关的头文件。*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/*  
+	CHASSIS_MODE_RELAX: Not force applied.
+	CHASSIS_MODE_BREAK: Set to zero speed. Force applied.
+	CHASSIS_MODE_FOLLOW_GIMBAL: Follow gimbal by follow encoder.
+	CHASSIS_MODE_INDENPENDENT: Run independently.
+	CHASSIS_MODE_OPEN: Direct apply force without pid control.
+*/
+
 typedef enum {
 	GIMBAL_MODE_RELAX,
 	GIMBAL_MODE_INIT,
 	GIMBAL_MODE_CALI,
-	GIMBAL_MODE_ABSOLUTE, 
-  GIMBAL_MODE_RELATIVE, 
-	GIMBAL_MODE_FIX,
+	GIMBAL_MODE_ABSOLUTE, /* Use IMU */
+	GIMBAL_MODE_RELATIVE, /* Use encoder */
+	GIMBAL_MODE_FIX, /* Use encoder */
 } Gimbal_Mode_t;
 
 typedef struct {
@@ -16,5 +37,9 @@ typedef struct {
 	
 } Gimbal_t;
 
-void Gimbal_Init(void);
 
+/* Exported functions prototypes ---------------------------------------------*/
+void Gimbal_Init(Gimbal_t *gimbal);
+void Gimbal_SetMode(Gimbal_t *gimbal);
+void Gimbal_Control(Gimbal_t *gimbal);
+void Gimbal_SetOutput(Gimbal_t *gimbal);
