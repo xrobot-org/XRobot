c505e9be4a173cef4689c2eade718d78e2d35cfa
Qu Shen <503578404@qq.com>
2020-02-19 22:10:59 +0800

    完成bmi088，但未调通

--- User/Devices/IMU/imu_bmi088.c
+++ User/Devices/IMU/imu_bmi088.c
@@ -84,7 +84,6 @@ static uint8_t buffer[2];
 static IMU_t *gimu;
 static bool inited = false;
 
-
 /* Private function  ---------------------------------------------------------*/
 static void BMI_WriteSingle(BMI_Device_t dv, uint8_t reg, uint8_t data) {
 	buffer[0] = (reg  & 0x7f);
@@ -182,14 +181,14 @@ void IMU_RxCpltCallback(void) {
 }
 
 void IMU_AcclIntCallback(void) {
-	BMI_Read(BMI_ACCL, BMI088_ACCL_X_LSB_REG, gimu->raw.bytes, 6u);
+	BMI_Read(BMI_ACCL, BMI088_ACCL_X_LSB_REG, gimu->raw, 6u);
 	/* WTF. Why seperate these 2 reg? */
 	//BMI_Read(BMI_ACCL, BMI088_TEMP_MSB_REG, gimu->raw.bytes + 6u, 2u);
 	
 }
 
 void IMU_GyroIntCallback(void) {
-	BMI_Read(BMI_ACCL, BMI088_GYRO_X_LSB_REG, gimu->raw.bytes + 8u, 6u);
+	BMI_Read(BMI_ACCL, BMI088_GYRO_X_LSB_REG, gimu->raw, 6u);
 }
 
 /* Exported functions --------------------------------------------------------*/
@@ -200,6 +199,7 @@ int IMU_Init(IMU_t *imu) {
 	if (inited)
 		return IMU_ERR_INITED;
 	
+	/* Switch accl to SPI mode. */
 	if (BMI_ReadSingle(BMI_ACCL, BMI088_ACCL_CHIP_ID_REG) != BMI088_ACCL_CHIP_ID)
 		return IMU_ERR_NO_DEV;
 	
@@ -207,16 +207,56 @@ int IMU_Init(IMU_t *imu) {
 		return IMU_ERR_NO_DEV;
 	
 	gimu = imu;
+	inited = true;
 	
 	/* BMI088 init. */
+	
+	/* Accl init. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_SOFTRESET_REG, 0xB6);
+	BSP_Delay(3);
+	
+	/* Filter setting: Normal. Output data rate 400. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_CONF_REG, 0xAA);
+	
+	/* 0x00: +-3G. 0x01: +-6G. 0x02: +-12G. 0x03: +-24G. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_RANGE_REG, 0x01);
+	
+	/* INT1 as output. Open-dtrin. Active High. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_INT1_IO_CONF_REG, 0x00);
+	
+	/* INT2 as output. Open-dtrin. Active High. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_INT2_IO_CONF_REG, 0x00);
+	
+	/* Map data ready interrupt to INT1 and INT2. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_INT1_INT2_MAP_DATA_REG, 0x44);
+	
+	/* Turn on accl. Now we can read data. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_PWR_CTRL_REG, 0x44);
+	BSP_Delay(50);
+	
+	/* Gyro init. */
+	BMI_WriteSingle(BMI_GYRO, BMI088_GYRO_SOFTRESET_REG, 0xB6);
+	BSP_Delay(3);
+	
+	/* 0x00: +-2000. 0x01: +-1000. 0x02: +-500. 0x03: +-250. 0x04: +-125. */
+	BMI_WriteSingle(BMI_GYRO, BMI088_GYRO_RANGE_REG, 0x00);
 
-	BSP_Delay(10);
-
+	/* Filter bw: 47Hz. Output data rate 400. */
+	BMI_WriteSingle(BMI_GYRO, BMI088_GYRO_RANGE_REG, 0x03);
+	
+	/* Map data ready interrupt to INT3 and INT4. */
+	BMI_WriteSingle(BMI_GYRO, BMI088_GYRO_INT_CTRL_REG, 0x80);
+	
+	/* INT3 and INT4 as output. Push-pull. Active low. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_ACCL_INT2_IO_CONF_REG, 0x00);
+	
+	/* Map data ready interrupt to INT3 and INT3. */
+	BMI_WriteSingle(BMI_ACCL, BMI088_GYRO_INT3_INT4_IO_MAP_REG, 0x81);
+	
 	BSP_SPI_RegisterCallback(BSP_SPI_IMU, BSP_SPI_RX_COMPLETE_CB, IMU_RxCpltCallback);
 	BSP_GPIO_RegisterCallback(ACCL_INT_Pin, IMU_AcclIntCallback);
 	BSP_GPIO_RegisterCallback(GYRO_INT_Pin, IMU_GyroIntCallback);
 	
-	inited = true;
 	return IMU_OK;
 }
 
@@ -229,34 +269,33 @@ IMU_t *IMU_GetDevice(void) {
 
 int IMU_StartReceiving(IMU_t *imu) {
 	//BMI_Read(BMI088_ACCL_XOUT_H, (uint8_t*)&(imu->raw), 20);
-	return 0;
+	return IMU_OK;
 }
 
-/* magn_scale[3] is initially zero. So data from uncalibrated magnentmeter is ignored. */
-int IMU_Parse(IMU_t *imu) {
-	if (imu == NULL)
-		return IMU_ERR_NULL;
+int IMU_ParseAccl(IMU_t *imu) {
+	uint16_t temp_uint11 = (imu->raw[0] << 3) | (imu->raw[1] >> 5);
 	
-	/* Convert endian. Processing of 4-bytes for one step. */
-	for(uint8_t i = 0; i < 5; i++) {
-		uint32_t value = (uint32_t)imu->raw.bytes[4 * i];
-		*(uint32_t*)(imu->raw.bytes + sizeof(uint32_t)) = (value & 0xFF00FF00) >> 8 | (value & 0x00FF00FF) << 8;
-	}
+	if(temp_uint11 > 1023)
+		temp_uint11 -= 2014;
+	
+	imu->temp = temp_uint11 * 0.125f + 21.f;
+	
+	imu->accl.x = (float)(imu->raw[1] | imu->raw[0]);
+	imu->accl.y = (float)(imu->raw[3] | imu->raw[2]);
+	imu->accl.z = (float)(imu->raw[5] | imu->raw[4]);
+	
+	return IMU_OK;
+}
 
-	imu->data.temp = 21.f + (float)imu->raw.packed.temp / 333.87f;
+int IMU_ParseGyro(IMU_t *imu) {
+	/* Gyroscope imu_raw -> degrees/sec -> radians/sec */
+	float raw_x = (float)(imu->raw[1] | imu->raw[0]);
+	float raw_y = (float)(imu->raw[3] | imu->raw[2]);
+	float raw_z = (float)(imu->raw[5] | imu->raw[4]);
 	
-	imu->data.accl.x = (float)imu->raw.packed.accl.x;
-	imu->data.accl.y = (float)imu->raw.packed.accl.y;
-	imu->data.accl.z = (float)imu->raw.packed.accl.z;
-	
-	/* Convert gyroscope imu_raw to degrees/sec, then, to radians/sec */
-	imu->data.gyro.x = (float)(imu->raw.packed.gyro.x - imu->cali.gyro_offset[0]) / 16.384f / 180.f * PI;
-	imu->data.gyro.y = (float)(imu->raw.packed.gyro.y - imu->cali.gyro_offset[1]) / 16.384f / 180.f * PI;
-	imu->data.gyro.z = (float)(imu->raw.packed.gyro.z - imu->cali.gyro_offset[2]) / 16.384f / 180.f * PI;
-	
-	imu->data.magn.x = (float)((imu->raw.packed.magn.x - imu->cali.magn_offset[0]) * imu->cali.magn_scale[0]);
-	imu->data.magn.y = (float)((imu->raw.packed.magn.y - imu->cali.magn_offset[1]) * imu->cali.magn_scale[1]);
-	imu->data.magn.z = (float)((imu->raw.packed.magn.z - imu->cali.magn_offset[2]) * imu->cali.magn_scale[2]);
+	imu->gyro.x = raw_x / 16.384f / 180.f * PI;
+	imu->gyro.y = raw_y / 16.384f / 180.f * PI;
+	imu->gyro.z = raw_z / 16.384f / 180.f * PI;
 	
 	return IMU_OK;
 }
