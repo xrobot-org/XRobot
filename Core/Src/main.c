48a1f1878d9fd45d778ba9a6825e6153f65d3328
Qu Shen <503578404@qq.com>
2020-02-26 23:17:55 +0800

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -233,7 +233,8 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+	while(1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -250,6 +251,9 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	
+	while(1) {
+	}
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
