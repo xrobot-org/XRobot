bf8340974d1fa690e8c8f2aea4c09e41df0cd2c2
Qu Shen <503578404@qq.com>
2020-02-27 10:14:04 +0800

    Update user_math.h

--- User/Components/user_math.h
+++ User/Components/user_math.h
@@ -8,9 +8,13 @@
 
 #include "stm32f4xx.h"
 
+#define ARM_MATH_CM4
 #include <arm_math.h>
+
 #include <float.h>
 
+#define MATH_DEGREE_TO_RADIAN_MULTIPLIER	(0.01745329251f)
+#define MATH_RADIAN_TO_DEGREE_MULTIPLIER	(57.2957795131f)
 
 float InvSqrt(float x);
 
