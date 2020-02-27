d1c7663ef7cd460b9bf3f195083492b3eae438c2
Qu Shen <503578404@qq.com>
2020-02-27 10:13:41 +0800

    bsp中添加gpio中断控制

--- User/Boards/DevC/bsp_gpio.c
+++ User/Boards/DevC/bsp_gpio.c
@@ -8,12 +8,13 @@
 /* Private macro -------------------------------------------------------------*/
 /* Private typedef -----------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-static struct {
-	void (*ACCL_INT_Pin_Callback)(void);
-	void (*GYRO_INT_Pin_Callback)(void);
-	void (*USER_KEY_Pin_Callback)(void);
+struct {
+	void(*ACCL_INT_Pin_Callback)(void);
+	void(*GYRO_INT_Pin_Callback)(void);
+	void(*USER_KEY_Pin_Callback)(void);
+	void(*CMPS_INT_Pin_Callback)(void);
 	/* void (*XXX_Pin_Callback)(void); */
-} bsp_gpio_callback;
+}static bsp_gpio_callback;
 
 /* Private function  ---------------------------------------------------------*/
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
@@ -31,7 +32,12 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 		case USER_KEY_Pin:
 			if (bsp_gpio_callback.USER_KEY_Pin_Callback)
 				bsp_gpio_callback.USER_KEY_Pin_Callback();
-		break;	
+			break;
+			
+		case CMPS_INT_Pin:
+			if (bsp_gpio_callback.CMPS_INT_Pin_Callback)
+				bsp_gpio_callback.CMPS_INT_Pin_Callback();
+			break;	
 		/*
 		case XXX_Pin:
 			if (bsp_gpio_callback.XXX_Pin_Callback)
@@ -62,9 +68,71 @@ int BSP_GPIO_RegisterCallback(uint16_t pin, void (*callback)(void)) {
 		case USER_KEY_Pin:
 			bsp_gpio_callback.USER_KEY_Pin_Callback = callback;
 			break;
+		
+		case CMPS_INT_Pin:
+			bsp_gpio_callback.CMPS_INT_Pin_Callback = callback;
+			break;
 		/*
 		case XXX_Pin:
 			bsp_gpio_callback.XXX_Pin_Callback = callback;
+			break;
+		*/
+		
+		default:
+			return -1;
+	}
+    return 0;
+}
+
+int BSP_GPIO_EnableIRQ(uint16_t pin) {
+    switch (pin) {
+		case ACCL_INT_Pin:
+			HAL_NVIC_EnableIRQ(ACCL_INT_EXTI_IRQn);
+			break;
+		
+		case GYRO_INT_Pin:
+			HAL_NVIC_EnableIRQ(GYRO_INT_EXTI_IRQn);
+			break;
+		
+		case USER_KEY_Pin:
+			HAL_NVIC_EnableIRQ(USER_KEY_EXTI_IRQn);
+			break;
+		
+		case CMPS_INT_Pin:
+			HAL_NVIC_EnableIRQ(CMPS_INT_EXTI_IRQn);
+			break;
+		/*
+		case XXX_Pin:
+			HAL_NVIC_EnableIRQ(XXX_IRQn);
+			break;
+		*/
+		
+		default:
+			return -1;
+	}
+    return 0;
+}
+
+int BSP_GPIO_DisableIRQ(uint16_t pin) {
+    switch (pin) {
+		case ACCL_INT_Pin:
+			HAL_NVIC_DisableIRQ(ACCL_INT_EXTI_IRQn);
+			break;
+		
+		case GYRO_INT_Pin:
+			HAL_NVIC_DisableIRQ(GYRO_INT_EXTI_IRQn);
+			break;
+		
+		case USER_KEY_Pin:
+			HAL_NVIC_DisableIRQ(USER_KEY_EXTI_IRQn);
+			break;
+		
+		case CMPS_INT_Pin:
+			HAL_NVIC_DisableIRQ(CMPS_INT_EXTI_IRQn);
+			break;
+		/*
+		case XXX_Pin:
+			HAL_NVIC_EnableIRQ(XXX_IRQn);
 			break;
 		*/
 		
--- User/Boards/bsp_gpio.h
+++ User/Boards/bsp_gpio.h
@@ -9,3 +9,6 @@
 /* Exported types ------------------------------------------------------------*/
 /* Exported functions prototypes ---------------------------------------------*/
 int BSP_GPIO_RegisterCallback(uint16_t pin, void (*callback)(void));
+
+int BSP_GPIO_EnableIRQ(uint16_t pin);
+int BSP_GPIO_DisableIRQ(uint16_t pin);
