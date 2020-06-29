9feec000167068be5dd8da1400d522737248cbc8
Qu Shen <503578404@qq.com>
2020-06-29 22:13:30 +0800

    完成bsp flash

--- User/Boards/bsp_flash.h
+++ User/Boards/bsp_flash.h
@@ -2,43 +2,46 @@
 
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdlib.h>
 #include <stdint.h>
 
 /* Exported constants --------------------------------------------------------*/
 /* Base address of the Flash sectors */
-#define ADDR_FLASH_SECTOR_0 ((uint32_t)0x08000000)  /* Base address of Sector 0, 16 Kbytes   */
-#define ADDR_FLASH_SECTOR_1 ((uint32_t)0x08004000)  /* Base address of Sector 1, 16 Kbytes   */
-#define ADDR_FLASH_SECTOR_2 ((uint32_t)0x08008000)  /* Base address of Sector 2, 16 Kbytes   */
-#define ADDR_FLASH_SECTOR_3 ((uint32_t)0x0800C000)  /* Base address of Sector 3, 16 Kbytes   */
-#define ADDR_FLASH_SECTOR_4 ((uint32_t)0x08010000)  /* Base address of Sector 4, 64 Kbytes   */
-#define ADDR_FLASH_SECTOR_5 ((uint32_t)0x08020000)  /* Base address of Sector 5, 128 Kbytes  */
-#define ADDR_FLASH_SECTOR_6 ((uint32_t)0x08040000)  /* Base address of Sector 6, 128 Kbytes  */
-#define ADDR_FLASH_SECTOR_7 ((uint32_t)0x08060000)  /* Base address of Sector 7, 128 Kbytes  */
-#define ADDR_FLASH_SECTOR_8 ((uint32_t)0x08080000)  /* Base address of Sector 8, 128 Kbytes  */
-#define ADDR_FLASH_SECTOR_9 ((uint32_t)0x080A0000)  /* Base address of Sector 9, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_0	((uint32_t)0x08000000)  /* Base address of Sector 0, 16 Kbytes   */
+#define ADDR_FLASH_SECTOR_1	((uint32_t)0x08004000)  /* Base address of Sector 1, 16 Kbytes   */
+#define ADDR_FLASH_SECTOR_2	((uint32_t)0x08008000)  /* Base address of Sector 2, 16 Kbytes   */
+#define ADDR_FLASH_SECTOR_3	((uint32_t)0x0800C000)  /* Base address of Sector 3, 16 Kbytes   */
+#define ADDR_FLASH_SECTOR_4	((uint32_t)0x08010000)  /* Base address of Sector 4, 64 Kbytes   */
+#define ADDR_FLASH_SECTOR_5	((uint32_t)0x08020000)  /* Base address of Sector 5, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_6	((uint32_t)0x08040000)  /* Base address of Sector 6, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_7	((uint32_t)0x08060000)  /* Base address of Sector 7, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_8	((uint32_t)0x08080000)  /* Base address of Sector 8, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_9	((uint32_t)0x080A0000)  /* Base address of Sector 9, 128 Kbytes  */
 #define ADDR_FLASH_SECTOR_10 ((uint32_t)0x080C0000) /* Base address of Sector 10, 128 Kbytes */
 #define ADDR_FLASH_SECTOR_11 ((uint32_t)0x080E0000) /* Base address of Sector 11, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_12 ((uint32_t)0x08100000) /* Base address of Sector 12, 16 Kbytes  */
-#define ADDR_FLASH_SECTOR_13 ((uint32_t)0x08104000) /* Base address of Sector 13, 16 Kbytes  */
-#define ADDR_FLASH_SECTOR_14 ((uint32_t)0x08108000) /* Base address of Sector 14, 16 Kbytes  */
-#define ADDR_FLASH_SECTOR_15 ((uint32_t)0x0810C000) /* Base address of Sector 15, 16 Kbytes  */
-#define ADDR_FLASH_SECTOR_16 ((uint32_t)0x08110000) /* Base address of Sector 16, 64 Kbytes  */
-#define ADDR_FLASH_SECTOR_17 ((uint32_t)0x08120000) /* Base address of Sector 17, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_18 ((uint32_t)0x08140000) /* Base address of Sector 18, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_19 ((uint32_t)0x08160000) /* Base address of Sector 19, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_20 ((uint32_t)0x08180000) /* Base address of Sector 20, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_21 ((uint32_t)0x081A0000) /* Base address of Sector 21, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_22 ((uint32_t)0x081C0000) /* Base address of Sector 22, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_23 ((uint32_t)0x081E0000) /* Base address of Sector 23, 128 Kbytes */
 
-#define FLASH_END_ADDR ((uint32_t)0x08100000)       /* Base address of Sector 23, 128 Kbytes */
+#ifndef FALSH_SIZE_LARGE
+#   define ADDR_FLASH_END 		((uint32_t)0x08100000)       /* End address for flash */
+#else
+#   define ADDR_FLASH_SECTOR_12 ((uint32_t)0x08100000) /* Base address of Sector 12, 16 Kbytes  */
+#   define ADDR_FLASH_SECTOR_13 ((uint32_t)0x08104000) /* Base address of Sector 13, 16 Kbytes  */
+#   define ADDR_FLASH_SECTOR_14 ((uint32_t)0x08108000) /* Base address of Sector 14, 16 Kbytes  */
+#   define ADDR_FLASH_SECTOR_15 ((uint32_t)0x0810C000) /* Base address of Sector 15, 16 Kbytes  */
+#   define ADDR_FLASH_SECTOR_16 ((uint32_t)0x08110000) /* Base address of Sector 16, 64 Kbytes  */
+#   define ADDR_FLASH_SECTOR_17 ((uint32_t)0x08120000) /* Base address of Sector 17, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_18 ((uint32_t)0x08140000) /* Base address of Sector 18, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_19 ((uint32_t)0x08160000) /* Base address of Sector 19, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_20 ((uint32_t)0x08180000) /* Base address of Sector 20, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_21 ((uint32_t)0x081A0000) /* Base address of Sector 21, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_22 ((uint32_t)0x081C0000) /* Base address of Sector 22, 128 Kbytes */
+#   define ADDR_FLASH_SECTOR_23 ((uint32_t)0x081E0000) /* Base address of Sector 23, 128 Kbytes */
+#   define ADDR_FLASH_END 		((uint32_t)0x08200000)       /* End address for flash */
+#endif
+
 
 /* Exported macro ------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
 /* Exported functions prototypes ---------------------------------------------*/
-void BSP_Flash_EraseAddress(uint32_t address, uint16_t len);
-int8_t BSP_Flash_WriteSingleAddress(uint32_t start_address, uint32_t *buf, uint32_t len);
-int8_t BSP_Flash_WriteMuliAddress(uint32_t start_address, uint32_t end_address, uint32_t *buf, uint32_t len);
-void BSP_FlashRead(uint32_t address, uint32_t *buf, uint32_t len);
-uint32_t BSP_Flash_GerSector(uint32_t address);
-uint32_t BSP_Flash_GetNextFlashAddress(uint32_t address);
+void BSP_Flash_EraseSectorAt(uint32_t address);
+void BSP_Flash_WriteBytes(uint32_t address, const uint8_t *buf, size_t len);
+void BSP_Flash_ReadBytes(uint32_t address, void *buf, size_t len);
