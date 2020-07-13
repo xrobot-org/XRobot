aa1dc271dfeff85ec1d4042c5febbd69f9fec7ff
Qu Shen <503578404@qq.com>
2020-07-14 00:40:02 +0800

    更准确的描述SOC自带CRC

--- User/Boards/DevC/bsp_crc32.c
+++ User/Boards/DevC/bsp_crc32.c
@@ -1,5 +1,5 @@
 /* Includes ------------------------------------------------------------------*/
-#include "bsp_crc.h"
+#include "bsp_crc32.h"
 
 #include "crc.h"
 
@@ -9,3 +9,14 @@
 /* Private variables ---------------------------------------------------------*/
 /* Private function  ---------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
+uint32_t BSP_GetCrc32CheckSum(uint32_t *data, uint32_t len) {
+	return HAL_CRC_Calculate(&hcrc, data, len) == HAL_OK;
+}
+
+bool BSP_VerifyCrc32CheckSum(uint32_t *data, uint32_t len) {
+	return 1;
+}
+
+bool BSP_AppendCrc32CheckSum(uint32_t *data, uint32_t len) {
+	return HAL_CRC_Accumulate(&hcrc, data, len) == HAL_OK;
+}
--- User/Boards/bsp_crc32.h
+++ User/Boards/bsp_crc32.h
@@ -11,4 +11,4 @@
 /* Exported functions prototypes ---------------------------------------------*/
 uint32_t BSP_GetCrc32CheckSum(uint32_t *data, uint32_t len);
 bool BSP_VerifyCrc32CheckSum(uint32_t *data, uint32_t len);
-void BSP_AppendCrc32CheckSum(uint32_t *data, uint32_t len);
+bool BSP_AppendCrc32CheckSum(uint32_t *data, uint32_t len);
