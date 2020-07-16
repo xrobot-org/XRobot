1a31a92ebe287de375553a1f12d2510fa310ae42
Qu Shen <503578404@qq.com>
2020-07-16 17:48:50 +0800

    调整CRC16标准，添加了CRC8

--- User/component/crc16.h
+++ User/component/crc16.h
@@ -1,5 +1,8 @@
 #pragma once
 
-#include <stdint.h>
+#include "user_math.h"
 
-uint16_t CRC16_Calc(const char *buf, int len);
+#define CRC16_INIT 0XFFFF
+
+uint16_t CRC16_Calc(const uint8_t *buf, size_t len, uint16_t crc);
+bool CRC16_Verify(const uint8_t *buf, size_t len);
---
+++ User/component/crc8.h
@@ -0,0 +1,12 @@
+/*
+	参考了Linux
+*/
+
+#pragma once
+
+#include "user_math.h"
+
+#define CRC8_INIT 0xFF
+
+uint8_t CRC8_Calc(const uint8_t *buf, size_t len, uint8_t crc);
+bool CRC8_Verify(const uint8_t *buf, size_t len);
