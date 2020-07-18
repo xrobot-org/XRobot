45f1a9223ce1815a628fb9e7e2baa2548ee14e93
Qu Shen <503578404@qq.com>
2020-07-18 14:13:23 +0800

    Update FreeRTOSConfig.h

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -67,7 +67,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)24576)
+#define configTOTAL_HEAP_SIZE                    ((size_t)0x6000)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configGENERATE_RUN_TIME_STATS            1
 #define configUSE_TRACE_FACILITY                 1
