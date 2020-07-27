21c8017d211eb15fe30ebbad0132e15bba87b8ab
Qu Shen <503578404@qq.com>
2020-07-27 15:29:14 +0800

    解决warning

--- User/bsp/c/crc32.c
+++ User/bsp/c/crc32.c
@@ -9,19 +9,19 @@
 /* Private variables ---------------------------------------------------------*/
 /* Private function  ---------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
-uint32_t BSP_CRC32_Calc(const uint8_t *buf, size_t len) {
+uint32_t BSP_CRC32_Calc(uint8_t *buf, size_t len) {
 	return HAL_CRC_Calculate(&hcrc, (uint32_t*)buf, len / sizeof(uint32_t));
 }
 
-bool BSP_CRC32_Verify(const uint8_t *buf, size_t len) {
+bool BSP_CRC32_Verify(uint8_t *buf, size_t len) {
 	if (len < 2)
 		return false;
 	
 	uint32_t expected = BSP_CRC32_Calc(buf, len / sizeof(uint32_t) - 1);
 	
-	return expected == ((const uint32_t*)buf)[len / sizeof(uint32_t) - 1];
+	return expected == ((uint32_t*)buf)[len / sizeof(uint32_t) - 1];
 }
 
-bool BSP_CRC32_Append(const uint8_t *buf, size_t len) {
+bool BSP_CRC32_Append(uint8_t *buf, size_t len) {
 	return HAL_CRC_Accumulate(&hcrc, (uint32_t*)buf, len / sizeof(uint32_t));
 }
--- User/bsp/crc32.h
+++ User/bsp/crc32.h
@@ -11,6 +11,6 @@
 /* Exported macro ------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
 /* Exported functions prototypes ---------------------------------------------*/
-uint32_t BSP_CRC32_Calc(const uint8_t *buf, size_t len);
-bool BSP_CRC32_Verify(const uint8_t *buf, size_t len);
-bool BSP_CRC32_Append(const uint8_t *buf, size_t len);
+uint32_t BSP_CRC32_Calc(uint8_t *buf, size_t len);
+bool BSP_CRC32_Verify(uint8_t *buf, size_t len);
+bool BSP_CRC32_Append(uint8_t *buf, size_t len);
