bc42cd4d551dc7b50c112f5f42ac51ce084c7ce6
Qu Shen <503578404@qq.com>
2020-07-27 15:30:04 +0800

    Update freertos.c

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -64,7 +64,7 @@ osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
   .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 512
+  .stack_size = 128 * 4
 };
 
 /* Private function prototypes -----------------------------------------------*/
