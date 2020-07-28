9537ba83a7c53880ab8924c7a41159154ca04bcf
Qu Shen <503578404@qq.com>
2020-07-28 12:27:10 +0800

    完成error detect

--- User/component/error_detect.c
+++ User/component/error_detect.c
@@ -4,6 +4,69 @@
 
 #include "error_detect.h"
 
-static void fun(void) {
-	return;
+#include <stddef.h>
+#include <string.h>
+
+#include "bsp/mm.h"
+
+static ErrorDetect_t ged;
+static bool inited = false;
+
+int8_t ErrorDetect_Init(void) {
+	if (inited)
+		return -1;
+	
+	memset(&ged, 0x00, sizeof(ged));
+	
+	for (uint8_t i = 0; i < ERROR_DETECT_UNIT_NUM; i++) {
+		ged.error[i].enable = true;
+		ged.error[i].priority = i;
+		ged.error[i].patient_lost = 500;
+		ged.error[i].patient_work = 500;
+	}
+	return 0;
+}
+
+void ErrorDetect_Processing(uint32_t sys_time) {
+	for (uint8_t i = 0; i < ERROR_DETECT_UNIT_NUM; i++) {
+		if (!ged.error[i].enable)
+			continue;
+		
+		if (sys_time - ged.error[i].showup > ged.error[i].patient_lost) {
+			ged.error[i].is_lost = true;
+			ged.error[i].found_lost = sys_time;
+		} else if (sys_time - ged.error[i].showup > ged.error[i].patient_lost) {
+			
+		} else {
+			ged.error[i].cycle_time = ged.error[i].showup - ged.error[i].showup_last;
+		}
+	}
+}
+
+bool ErrorDetect_ErrorExist(ErrorDetect_Unit_t unit) {
+	if (unit == ERROR_DETECT_UNIT_NO_DEV) {
+		for (uint8_t i = ERROR_DETECT_UNIT_NUM; i > 0; i--) {
+			if (ged.error[i].error_exist)
+				return true;
+		}
+		return false;
+	} else {
+		return ged.error[unit].error_exist;
+	}
+}
+
+ErrorDetect_Unit_t ErrorDetect_GetErrorUnit(void) {
+	for (uint8_t i = ERROR_DETECT_UNIT_NUM; i > 0; i--) {
+		if (ged.error[i].error_exist)
+			return i;
+	}
+		return ERROR_DETECT_UNIT_NO_DEV;
+}
+
+const ErrorDetect_Error_t *ErrorDetect_GetDetail(ErrorDetect_Unit_t unit){
+	return &ged.error[unit];
+}
+
+void ErrorDetect_Update(ErrorDetect_Unit_t unit, uint32_t time_current) {
+	ged.error[unit].showup = time_current;
 }
--- User/component/error_detect.h
+++ User/component/error_detect.h
@@ -8,37 +8,51 @@
 #include <stdint.h>
 
 typedef enum {
-    DBUS = 0,
-    CHASSIS_M1,
-    CHASSIS_M2,
-    CHASSIS_M3,
-    CHASSIS_M4,
-    GIMBAL_YAW,
-    GIMBAL_PIT,
-    TRIGGER,
-    FEED,
-    GYRO,
-    ACCL,
-    MAGN,
-    REFEREE,
-    ERROR_NUM,
-} ErrorDetect_ErrorType_t;
+	/* Low priority */
+	ERROR_DETECT_UNIT_NO_DEV = 0,
+	ERROR_DETECT_UNIT_REFEREE,
+	ERROR_DETECT_UNIT_CHASSIS_M1,
+	ERROR_DETECT_UNIT_CHASSIS_M2,
+	ERROR_DETECT_UNIT_CHASSIS_M3,
+	ERROR_DETECT_UNIT_CHASSIS_M4,
+	ERROR_DETECT_UNIT_TRIGGER,
+	ERROR_DETECT_UNIT_FEED,
+	ERROR_DETECT_UNIT_GIMBAL_YAW,
+	ERROR_DETECT_UNIT_GIMBAL_PIT,
+	ERROR_DETECT_UNIT_GYRO,
+	ERROR_DETECT_UNIT_ACCL,
+	ERROR_DETECT_UNIT_MAGN,
+	ERROR_DETECT_UNIT_DBUS,
+	ERROR_DETECT_UNIT_NUM,
+	/* High priority */
+} ErrorDetect_Unit_t;
 
 typedef struct {
-    uint32_t time_current;
-    uint32_t time_last;
-    uint32_t period_lost;
-    uint32_t period_work;
-    uint16_t set_offline_time : 12;
-    uint16_t set_online_time : 12;
-    uint8_t enable : 1;
-    uint8_t priority : 4;
-    uint8_t error_exist : 1;
-    uint8_t is_lost : 1;
-    uint8_t data_is_error : 1;
+	bool enable;
+	uint8_t priority;
+	uint32_t patient_lost;
+	uint32_t patient_work;
+	
+	uint32_t showup;
+	uint32_t showup_last;
+	uint32_t cycle_time;
+	uint32_t duration_lost;
+	uint32_t duration_work;
+	uint32_t found_lost;
+	bool error_exist;
+	bool is_lost;
+	uint8_t data_is_error;
 
-    float frequency;
-    bool (*data_is_error_fun)(void);
-    void (*solve_lost_fun)(void);
-    void (*solve_data_error_fun)(void);
 } ErrorDetect_Error_t;
+
+typedef struct {
+	ErrorDetect_Error_t error[ERROR_DETECT_UNIT_NUM];
+} ErrorDetect_t;
+
+int8_t ErrorDetect_Init(void);
+void ErrorDetect_Processing(uint32_t sys_time);
+bool ErrorDetect_ErrorExist(ErrorDetect_Unit_t unit);
+ErrorDetect_Unit_t ErrorDetect_GetErrorUnit(void);
+const ErrorDetect_Error_t *ErrorDetect_GetDetail(ErrorDetect_Unit_t unit);
+
+void ErrorDetect_Update(ErrorDetect_Unit_t unit, uint32_t time_current);
