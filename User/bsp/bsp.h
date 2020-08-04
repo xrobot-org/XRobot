e0b4ea8b9183004be153bfde96e22807324f3605
Qu Shen <503578404@qq.com>
2020-08-04 21:25:24 +0800

    添加统一的bsp头文件

---
+++ User/bsp/bsp.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#define BSP_OK			(0)
+#define BSP_ERR			(-1)
+#define BSP_ERR_NULL	(-2)
+#define BSP_ERR_INITED	(-3)
+#define BSP_ERR_NO_DEV	(-4)
+
+#define SIGNAL_BSP_USB_BUF_RECV (1u<<0)
