e7a2e0a4a2924c1676171acdedc7d584eb165aa5
Qu Shen <503578404@qq.com>
2020-08-08 10:52:15 +0800

    Update user_task.c

--- User/task/user_task.c
+++ User/task/user_task.c
@@ -1,50 +1,50 @@
 /* Includes ------------------------------------------------------------------*/
 #include "user_task.h"
 
-const osThreadAttr_t attr_command= {
-  .name = "command",
-  .priority = (osPriority_t) osPriorityHigh,
-  .stack_size = 128 * 4,
+const osThreadAttr_t attr_command = {
+	.name = "command",
+	.priority = (osPriority_t) osPriorityHigh,
+	.stack_size = 128 * 4,
 };
 
-const osThreadAttr_t attr_ctrl_chassis= {
-  .name = "ctrl_chassis",
-  .priority = (osPriority_t) osPriorityAboveNormal,
-  .stack_size = 256 * 4,
+const osThreadAttr_t attr_ctrl_chassis = {
+	.name = "ctrl_chassis",
+	.priority = (osPriority_t) osPriorityAboveNormal,
+	.stack_size = 256 * 4,
 };
 
-const osThreadAttr_t attr_ctrl_gimbal= {
-  .name = "ctrl_gimbal",
-  .priority = (osPriority_t) osPriorityAboveNormal,
-  .stack_size = 256 * 4,
+const osThreadAttr_t attr_ctrl_gimbal = {
+	.name = "ctrl_gimbal",
+	.priority = (osPriority_t) osPriorityAboveNormal,
+	.stack_size = 256 * 4,
 };
 
-const osThreadAttr_t attr_ctrl_shoot= {
-  .name = "ctrl_shoot",
-  .priority = (osPriority_t) osPriorityAboveNormal,
-  .stack_size = 256 * 4,
+const osThreadAttr_t attr_ctrl_shoot = {
+	.name = "ctrl_shoot",
+	.priority = (osPriority_t) osPriorityAboveNormal,
+	.stack_size = 256 * 4,
 };
 
-const osThreadAttr_t attr_info= {
-  .name = "info",
-  .priority = (osPriority_t) osPriorityBelowNormal,
-  .stack_size = 128 * 4,
+const osThreadAttr_t attr_info = {
+	.name = "info",
+	.priority = (osPriority_t) osPriorityBelowNormal,
+	.stack_size = 128 * 4,
 };
 
-const osThreadAttr_t attr_monitor= {
-  .name = "monitor",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4,
+const osThreadAttr_t attr_monitor = {
+	.name = "monitor",
+	.priority = (osPriority_t) osPriorityNormal,
+	.stack_size = 128 * 4,
 };
 
-const osThreadAttr_t attr_atti_esti= {
-  .name = "atti_esti",
-  .priority = (osPriority_t) osPriorityRealtime,
-  .stack_size = 256 * 4,
+const osThreadAttr_t attr_atti_esti = {
+	.name = "atti_esti",
+	.priority = (osPriority_t) osPriorityRealtime,
+	.stack_size = 256 * 4,
 };
 
-const osThreadAttr_t attr_referee= {
-  .name = "referee",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4,
+const osThreadAttr_t attr_referee = {
+	.name = "referee",
+	.priority = (osPriority_t) osPriorityNormal,
+	.stack_size = 128 * 4,
 };
