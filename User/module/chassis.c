a7be546b02eb2973b2bb18fad485d15e82649521
Qu Shen <503578404@qq.com>
2020-09-16 14:51:30 +0800

    优化错误处理

--- User/module/chassis.c
+++ User/module/chassis.c
@@ -49,13 +49,16 @@ static int8_t Chassis_SetMode(Chassis_t *c, CMD_Chassis_Mode_t mode) {
 int8_t Chassis_Init(Chassis_t *c, const Chassis_Params_t *param, float dt_sec) {
   if (c == NULL) return CHASSIS_ERR_NULL;
 
+  /* 初始化参数 */
   c->param = param;
   c->dt_sec = dt_sec;
 
+  /* 设置默认模式 */
   c->mode = CHASSIS_MODE_RELAX;
   
+  /* 根据参数（param）中的底盘型号初始化Mixer */
   Mixer_Mode_t mixer_mode;
-  switch (param->type) {
+  switch (c->param->type) {
     case CHASSIS_TYPE_MECANUM:
       c->num_wheel = 4;
       mixer_mode = MIXER_MECANUM;
@@ -85,53 +88,50 @@ int8_t Chassis_Init(Chassis_t *c, const Chassis_Params_t *param, float dt_sec) {
       // onboard sdk.
       return CHASSIS_ERR_TYPE;
   }
-
+  
+  /* 根据底盘型号动态分配控制时使用的变量 */
   c->feedback.motor_rpm =
       BSP_Malloc((size_t)c->num_wheel * sizeof(*c->feedback.motor_rpm));
-  if (c->feedback.motor_rpm == NULL) goto error1;
+  if (c->feedback.motor_rpm == NULL) goto error;
 
   c->set_point.motor_rpm =
       BSP_Malloc((size_t)c->num_wheel * sizeof(*c->set_point.motor_rpm));
-  if (c->set_point.motor_rpm == NULL) goto error2;
+  if (c->set_point.motor_rpm == NULL) goto error;
 
   c->pid.motor = BSP_Malloc((size_t)c->num_wheel * sizeof(*c->pid.motor));
-  if (c->pid.motor == NULL) goto error3;
+  if (c->pid.motor == NULL) goto error;
 
   c->out = BSP_Malloc((size_t)c->num_wheel * sizeof(*c->out));
-  if (c->out == NULL) goto error4;
+  if (c->out == NULL) goto error;
 
   c->filter = BSP_Malloc((size_t)c->num_wheel * sizeof(*c->filter));
-  if (c->filter == NULL) goto error5;
+  if (c->filter == NULL) goto error;
 
   for (uint8_t i = 0; i < c->num_wheel; i++) {
     PID_Init(&(c->pid.motor[i]), PID_MODE_NO_D, c->dt_sec,
-             &(param->motor_pid_param[i]));
+             &(c->param->motor_pid_param[i]));
 
     LowPassFilter2p_Init(&(c->filter[i]), 1.f / c->dt_sec,
-                         param->low_pass_cutoff);
+                         c->param->low_pass_cutoff);
   }
 
   PID_Init(&(c->pid.follow), PID_MODE_NO_D, c->dt_sec,
-           &(param->follow_pid_param));
+           &(c->param->follow_pid_param));
 
   Mixer_Init(&(c->mixer), mixer_mode);
   return CHASSIS_OK;
 
-error5:
+error:
+  /* 动态内存分配错误时，释放已经分配的内存，返回错误值 */
   BSP_Free(c->out);
-error4:
   BSP_Free(c->pid.motor);
-error3:
   BSP_Free(c->set_point.motor_rpm);
-error2:
   BSP_Free(c->feedback.motor_rpm);
-error1:
   return CHASSIS_ERR_NULL;
 }
 
 int8_t Chassis_UpdateFeedback(Chassis_t *c, CAN_t *can) {
   if (c == NULL) return CHASSIS_ERR_NULL;
-
   if (can == NULL) return CHASSIS_ERR_NULL;
 
   c->feedback.gimbal_yaw_angle =
@@ -146,7 +146,6 @@ int8_t Chassis_UpdateFeedback(Chassis_t *c, CAN_t *can) {
 
 int8_t Chassis_Control(Chassis_t *c, CMD_Chassis_Ctrl_t *c_ctrl) {
   if (c == NULL) return CHASSIS_ERR_NULL;
-
   if (c_ctrl == NULL) return CHASSIS_ERR_NULL;
 
   Chassis_SetMode(c, c_ctrl->mode);
