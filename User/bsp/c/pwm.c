5d99c7dc402605a1d2fe06c083c83348514fce59
Qu Shen <503578404@qq.com>
2020-09-21 21:01:42 +0800

    PWM输入小于0时关闭

--- User/bsp/c/pwm.c
+++ User/bsp/c/pwm.c
@@ -21,13 +21,18 @@ int8_t BSP_PWM_Start(BSP_PWM_Channel_t ch) {
 
 int8_t BSP_PWM_Set(BSP_PWM_Channel_t ch, float duty_cycle) {
   if (duty_cycle > 1.0f) return BSP_ERR;
+  if (duty_cycle < 0.0f) duty_cycle = 0.f;
 
   uint16_t pulse = (uint16_t)(duty_cycle * (float)UINT16_MAX);
 
-  switch (ch) {
-    case BSP_PWM_IMU_HEAT:
-      __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
-      break;
+  if (pulse > 0) {
+    switch (ch) {
+      case BSP_PWM_IMU_HEAT:
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
+        break;
+    }
+  } else {
+    BSP_PWM_Stop(ch);
   }
   return BSP_OK;
 }
