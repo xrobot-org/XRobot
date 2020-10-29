e1bef824abcbed35887a350112bd29c5079b50eb
Qu Shen <503578404@qq.com>
2020-10-29 15:55:31 +0800

    user_math中添加部分注释,修复CircleError错误

--- User/component/user_math.c
+++ User/component/user_math.c
@@ -29,28 +29,52 @@ inline float AbsClip(float in, float limit) {
 
 inline float Sign(float in) { return (in > 0) ? 1.0f : 0.0f; }
 
+/*!
+ * \brief 将运动向量置零
+ *
+ * \param mv 被操作的值
+ */
 inline void ResetMoveVector(MoveVector_t *mv) {
   memset(mv, 0, sizeof(MoveVector_t));
 }
 
+/*!
+ * \brief 计算循环值的误差，用于没有负数值，并在一定范围内变化的值
+ * 例如编码器：相差1.5PI其实等于相差-0.5PI
+ *
+ * \param sp 被操作的值
+ * \param fb 变化量
+ * \param range 被操作的值变化范围，正数时起效
+ *
+ * \return 函数运行结果
+ */
 inline float CircleError(float sp, float fb, float range) {
   float error = sp - fb;
   if (range > 0.0f) {
     float half_range = range / 2.0f;
-    if (error > half_range) {
-      error -= half_range;
-      return -error;
-    }
+    if (error > half_range)
+      error -= range;
+    else if (error < -half_range)
+      error += range;
   }
   return error;
 }
 
+/*!
+ * \brief 循环加法，用于没有负数值，并在一定范围内变化的值
+ * 例如编码器，在0-2PI内变化，1.5PI + 1.5PI = 1PI
+ *
+ * \param origin 被操作的值
+ * \param delta 变化量
+ * \param range 被操作的值变化范围，正数时起效
+ */
 inline void CircleAdd(float *origin, float delta, float range) {
   float out = *origin + delta;
-  if (out > range)
-    out -= range;
-  else if (out < 0.0f)
-    out += range;
-
+  if (range > 0.0f) {
+    if (out >= range)
+      out -= range;
+    else if (out < 0.0f)
+      out += range;
+  }
   *origin = out;
 }
