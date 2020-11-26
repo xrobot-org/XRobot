40eed12d1a035116a6318179877ead5e3427febe
Qu Shen <503578404@qq.com>
2020-11-26 20:35:49 +0800

    修改PowerLimit_Apply参数名

--- User/component/limiter.c
+++ User/component/limiter.c
@@ -16,26 +16,26 @@ int8_t HeatLimiter_Apply(float heat_limit, float vbat, float dt_sec) {
 
 /**
  * @brief 底盘功率限制
- * 
+ *
  * @param power_limit 功率阈值
  * @param vbat 电池电压
- * @param motor 电机输出数组
- * @param last_motor 上次电机输出数组
- * @param len 电机输出数组长度
+ * @param motor_out 电机输出值数组
+ * @param cur_fb 电机电流反馈值数组
+ * @param len 数组长度
  * @return int8_t 0对应没有错误
  */
-int8_t PowerLimit_Apply(float power_limit, float vbat, float *motor,
-                        float *last_motor, uint32_t len) {
-  if (motor == NULL) return -1;
+int8_t PowerLimit_Apply(float power_limit, float vbat, float *motor_out,
+                        float *cur_fb, uint32_t len) {
+  if (motor_out == NULL) return -1;
   float last_total_current = 0.0f;
 
   for (uint32_t i = 0; i < len; i++)
-    last_total_current += fabsf(last_motor[i]); /* 检测功率是否超出限制 */
+    last_total_current += fabsf(cur_fb[i]); /* 检测功率是否超出限制 */
   if (power_limit > last_total_current * vbat) return -2;
 
   float total_current = 0.0f;
   for (uint32_t i = 0; i < len; i++) {
-    total_current += fabsf(motor[i]);
+    total_current += fabsf(motor_out[i]);
   }
 
   if (power_limit > 0.0f) {
@@ -43,7 +43,7 @@ int8_t PowerLimit_Apply(float power_limit, float vbat, float *motor,
       float current_scale =
           power_limit / vbat / total_current; /* 保持每个电机输出值比例不变 */
       for (uint32_t i = 0; i < len; i++) {
-        motor[i] *= current_scale;
+        motor_out[i] *= current_scale;
       }
     }
   }
