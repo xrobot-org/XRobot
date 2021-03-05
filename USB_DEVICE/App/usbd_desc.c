d7040ecae50e6c2f1156836dffa7fcde4759c42b
Qu Shen <503578404@qq.com>
2021-03-06 01:27:40 +0800

    修改USB描述

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -63,11 +63,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0xFFFC
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "QDU RM"
+#define USBD_PID_FS     8888
+#define USBD_PRODUCT_STRING_FS     "QDU RM MCU CLI"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
