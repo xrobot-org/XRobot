950ddd491b100399f717daf67c8bb2ec941bed20
Qu Shen <503578404@qq.com>
2021-03-28 13:26:49 +0800

    gimbal_yaw_angle -> gimbal_yaw_encoder

--- User/module/chassis.h
+++ User/module/chassis.h
@@ -72,7 +72,7 @@ typedef struct {
   MoveVector_t move_vec; /* 底盘实际的运动向量 */
 
   struct {
-    float gimbal_yaw_angle; /* 云台Yaw轴编码器角度 */
+    float gimbal_yaw_encoder; /* 云台Yaw轴编码器角度 */
     float *motor_rpm;       /* 电机转速的动态数组，单位：RPM */
     float *motor_current;   /* 转矩电流 单位：A */
   } feedback;               /* 反馈信息 */
