4ecad9dc87a63e8185f3f535c43b68977480ea7a
Qu Shen <503578404@qq.com>
2021-04-29 18:13:23 +0800

    修复Mixer中的bug 优化错误解决逻辑

--- User/component/mixer.c
+++ User/component/mixer.c
@@ -4,8 +4,6 @@
 
 #include "mixer.h"
 
-#include "math.h"
-
 /**
  * @brief 初始化混合器
  *
@@ -33,44 +31,43 @@ int8_t Mixer_Init(Mixer_t *mixer, Mixer_Mode_t mode) {
 int8_t Mixer_Apply(Mixer_t *mixer, MoveVector_t *move_vec, float *out,
                    size_t len, float scale_factor) {
   ASSERT(mixer);
+  ASSERT(move_vec);
+  ASSERT(out);
 
   switch (mixer->mode) {
     case MIXER_MECANUM:
-      if (len == 4) {
-        out[0] = move_vec->vx - move_vec->vy + move_vec->wz;
-        out[1] = move_vec->vx + move_vec->vy + move_vec->wz;
-        out[2] = -move_vec->vx + move_vec->vy + move_vec->wz;
-        out[3] = -move_vec->vx - move_vec->vy + move_vec->wz;
-      } else {
-        goto error;
-      }
+      ASSERT(len == 4);
+      out[0] = move_vec->vx - move_vec->vy + move_vec->wz;
+      out[1] = move_vec->vx + move_vec->vy + move_vec->wz;
+      out[2] = -move_vec->vx + move_vec->vy + move_vec->wz;
+      out[3] = -move_vec->vx - move_vec->vy + move_vec->wz;
       break;
 
     case MIXER_PARLFIX4:
-      if (len == 4) {
-        out[0] = -move_vec->vx;
-        out[1] = move_vec->vx;
-        out[2] = move_vec->vx;
-        out[3] = -move_vec->vx;
-      } else {
-        goto error;
-      }
+      ASSERT(len == 4);
+      out[0] = -move_vec->vx;
+      out[1] = move_vec->vx;
+      out[2] = move_vec->vx;
+      out[3] = -move_vec->vx;
+      break;
+
     case MIXER_PARLFIX2:
-      if (len == 2) {
-        out[0] = -move_vec->vx;
-        out[1] = move_vec->vx;
-      } else {
-        goto error;
-      }
+      ASSERT(len == 2);
+      out[0] = -move_vec->vx;
+      out[1] = move_vec->vx;
+      break;
+
     case MIXER_SINGLE:
-      if (len == 1) {
-        out[0] = move_vec->vx;
-      } else {
-        goto error;
-      }
+      ASSERT(len == 1);
+      out[0] = move_vec->vx;
+      break;
+
     case MIXER_OMNICROSS:
     case MIXER_OMNIPLUS:
-      goto error;
+      break;
+
+    default:
+      break;
   }
 
   float abs_max = 0.f;
@@ -87,8 +84,4 @@ int8_t Mixer_Apply(Mixer_t *mixer, MoveVector_t *move_vec, float *out,
     out[i] *= scale_factor;
   }
   return 0;
-
-error:
-  for (size_t i = 0; i < len; i++) out[i] = 0;
-  return -1;
 }
