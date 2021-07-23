f59110d1758ccdadc8cecdeb13c20f84281869e7
刘骢 <2592509183@qq.com>
2021-07-24 04:21:44 +0800

    更改上位机通信波特率

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -94,7 +94,7 @@ void MX_USART6_UART_Init(void) {
 
   /* USER CODE END USART6_Init 1 */
   huart6.Instance = USART6;
-  huart6.Init.BaudRate = 115200;
+  huart6.Init.BaudRate = 460800;
   huart6.Init.WordLength = UART_WORDLENGTH_8B;
   huart6.Init.StopBits = UART_STOPBITS_1;
   huart6.Init.Parity = UART_PARITY_NONE;
