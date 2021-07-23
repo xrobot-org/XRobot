993e63ad9bc008e5097465f1f765d0370080c0db
刘骢 <2592509183@qq.com>
2021-07-24 04:26:50 +0800

    调整任务运行频率

--- User/task/user_task.h
+++ User/task/user_task.h
@@ -23,13 +23,13 @@
 /* 所有任务都要define一个“任务运行频率”和“初始化延时” */
 #define TASK_FREQ_CTRL_CHASSIS (500u)
 #define TASK_FREQ_CTRL_GIMBAL (500u)
-#define TASK_FREQ_CTRL_LAUNCHER (500)
+#define TASK_FREQ_CTRL_LAUNCHER (500u)
 #define TASK_FREQ_CTRL_CAP (100u)
 #define TASK_FREQ_CTRL_COMMAND (500u)
 #define TASK_FREQ_INFO (4u)
 #define TASK_FREQ_MONITOR (2u)
 #define TASK_FREQ_CAN (1000u)
-#define TASK_FREQ_AI (250u)
+#define TASK_FREQ_AI (500u)
 #define TASK_FREQ_REFEREE (1000u)
 
 /* Exported defines --------------------------------------------------------- */
