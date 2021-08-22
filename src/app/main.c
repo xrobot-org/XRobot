6849862ff81a74894f7dc57c68bfb38a33edea9c
Qu Shen <503578404@qq.com>
2021-08-22 13:14:07 +0800

    去掉多余注释，移动HAL API到对应文件夹

--- src/app/main.c
+++ src/app/main.c
@@ -14,117 +14,25 @@
  * @retval int
  */
 int main(void) {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick.
    */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_ADC3_Init();
-  MX_CAN1_Init();
-  MX_CAN2_Init();
-  MX_I2C1_Init();
-  MX_SPI1_Init();
-  MX_TIM4_Init();
-  MX_TIM5_Init();
-  MX_USART3_UART_Init();
-  MX_TIM8_Init();
-  MX_CRC_Init();
-  MX_RNG_Init();
-  MX_I2C2_Init();
-  MX_I2C3_Init();
-  MX_SPI2_Init();
-  MX_TIM1_Init();
-  MX_TIM3_Init();
-  MX_TIM10_Init();
-  MX_USART1_UART_Init();
-  MX_USART6_UART_Init();
-  MX_TIM7_Init();
-  MX_DCMI_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
+  HAL_InitPeripherals();
 
   /* Init scheduler */
-  osKernelInitialize(); /* Call init function for freertos objects (in
-                           freertos.c) */
-  MX_FREERTOS_Init();
+
   /* Start scheduler */
-  osKernelStart();
+  vTaskStartScheduler();
 
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1) {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
-
-/**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-   */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 6;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-    Error_Handler();
   }
 }
-
-/* USER CODE BEGIN 4 */
-/* High freq timer ticks for runtime stats */
-extern unsigned long high_freq_timer_ticks;
-/* USER CODE END 4 */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
