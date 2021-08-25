405160116652e11e9987e4d945bb6fbb25790463
Qu Shen <503578404@qq.com>
2021-08-25 22:46:27 +0800

    完成USB库

--- src/usb/usbd_desc.h
+++ src/usb/usbd_desc.h
@@ -18,8 +18,12 @@
  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __USBD_DESC_H
-#define __USBD_DESC_H
+#ifndef __USBD_DESC__C__
+#define __USBD_DESC__C__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "usbd_def.h"
@@ -27,39 +31,18 @@
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 #define DEVICE_ID1 (UID_BASE)
-#define DEVICE_ID2 (UID_BASE + 0x4U)
-#define DEVICE_ID3 (UID_BASE + 0x8U)
+#define DEVICE_ID2 (UID_BASE + 0x4)
+#define DEVICE_ID3 (UID_BASE + 0x8)
 
-/*
- * USB Billboard Class USER string desc Defines
- * index should start form 0x10 to avoid using the reserved device string desc
- * indexes
- */
-#if (USBD_CLASS_USER_STRING_DESC == 1)
-#define USBD_BB_IF_STRING_INDEX 0x10U
-#define USBD_BB_URL_STRING_INDEX 0x11U
-#define USBD_BB_ALTMODE0_STRING_INDEX 0x12U
-#define USBD_BB_ALTMODE1_STRING_INDEX 0x13U
-/* Add Specific USER string Desc */
-#define USBD_BB_IF_STR_DESC (uint8_t *)"STM32 BillBoard Interface"
-#define USBD_BB_URL_STR_DESC (uint8_t *)"www.st.com"
-#define USBD_BB_ALTMODE0_STR_DESC (uint8_t *)"STM32 Alternate0 Mode"
-#define USBD_BB_ALTMODE1_STR_DESC (uint8_t *)"STM32 Alternate1 Mode"
+#define USB_SIZ_STRING_SERIAL 0x1A
+
+/** Descriptor for the Usb device. */
+extern USBD_DescriptorsTypeDef CDC_Desc;
+
+#ifdef __cplusplus
+}
 #endif
 
-#define USB_SIZ_STRING_SERIAL 0x1AU
-
-#if (USBD_LPM_ENABLED == 1)
-#define USB_SIZ_BOS_DESC 0x0CU
-#elif (USBD_CLASS_BOS_ENABLED == 1)
-#define USB_SIZ_BOS_DESC 0x5DU
-#endif
-
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
-extern USBD_DescriptorsTypeDef CDC_Desc; /* Replace 'XXX_Desc' with your active
-                                            USB device class, ex: HID_Desc */
-
-#endif /* __USBD_DESC_H*/
+#endif /* __USBD_DESC__C__ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
