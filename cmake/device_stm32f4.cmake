1e04bdf1e83d02608b1de2c0f3017e0cf7334271
Qu Shen <503578404@qq.com>
2021-08-25 22:46:16 +0800

    优化工具链flag

--- cmake/device_stm32f4.cmake
+++ cmake/device_stm32f4.cmake
@@ -1,13 +1,12 @@
 # Architecture option
-set(CPU "-mcpu=cortex-m4")
-set(ENDIAN "-mlittle-endian")
+set(CPU "-mcpu=cortex-m4")  
 set(FPU "-mfpu=fpv4-sp-d16")
 set(FLOAT_ABI "-mfloat-abi=hard")
 
-set(MCU_FLAGS "${CPU} ${ENDIAN} -mthumb ${FPU} ${FLOAT_ABI}")
+set(MCU_FLAGS "${CPU} -mthumb ${FPU} ${FLOAT_ABI}")
 
 # TODO: Remove "-w" & Add "-Wall -Wextra"
-set(GENERAL_FLAGS "-fno-builtin -w -ffunction-sections -fdata-sections -mabi=aapcs")
+set(GENERAL_FLAGS "-w -fno-builtin -ffunction-sections -fdata-sections -mabi=aapcs")
 
 set(CMAKE_C_FLAGS "${MCU_FLAGS} ${GENERAL_FLAGS}")
 set(CMAKE_ASM_FLAGS "${MCU_FLAGS} ${GENERAL_FLAGS} -x assembler-with-cpp")
@@ -17,7 +16,7 @@ set(LINKER_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/ld/STM32F407IGHx_FLASH.ld)
 
 # Linker Flag
 set(LINK_LIB "-lc -lm -lnosys")
-set(CMAKE_EXE_LINKER_FLAGS "${MCU_FLAGS} --specs=nosys.specs --specs=nano.specs -mabi=aapcs -T ${LINKER_SCRIPT} ${LINK_LIB} -Wl,-Map=${CMAKE_PROJECT_NAME}.map,--cref,--gc-sections")
+set(CMAKE_EXE_LINKER_FLAGS "${MCU_FLAGS} ${GENERAL_FLAGS} --specs=nosys.specs --specs=nano.specs -mabi=aapcs -T ${LINKER_SCRIPT} ${LINK_LIB} -Wl,-Map=${CMAKE_PROJECT_NAME}.map,--cref,--gc-sections")
 
 # 处理器相关宏定义
 add_compile_definitions(
