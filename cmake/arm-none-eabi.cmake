f4ae2d0d1a20efe576209fdbcada707faf783e5e
Qu Shen <503578404@qq.com>
2021-08-28 13:17:53 +0800

    添加C++编译器，调增AR gcc-ar，debug等级改为g3

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -6,8 +6,9 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 
 # Toolchain settings
 set(CMAKE_C_COMPILER    arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER  arm-none-eabi-g++)
 set(CMAKE_ASM_COMPILER  arm-none-eabi-gcc)
-set(AR                  arm-none-eabi-ar)
+set(AR                  arm-none-eabi-gcc-ar)
 set(OBJCOPY             arm-none-eabi-objcopy)
 set(OBJDUMP             arm-none-eabi-objdump)
 set(SIZE                arm-none-eabi-size)
@@ -24,14 +25,11 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # General Compiler Options
 
 # Options for DEBUG build
-# -Og enables optimizations that do not interfere with debugging
-# -g produce debugging information in the operating system’s native format
-set(CMAKE_C_FLAGS_DEBUG "-Og -g -ggdb3")
-set(CMAKE_ASM_FLAGS_DEBUG "-g -ggdb3")
+set(CMAKE_C_FLAGS_DEBUG "-Og -g3 -ggdb3")
+set(CMAKE_ASM_FLAGS_DEBUG "-g3 -ggdb3")
 set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
 
 # Options for RELEASE build
-# -Os Optimize for size. -Os enables all -O2 optimizations
 set(CMAKE_C_FLAGS_RELEASE "-Os")
 set(CMAKE_ASM_FLAGS_RELEASE "")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
