0197b5e0e10ebf5ab0d03d0a5e4b0fba2681d372
Qu Shen <503578404@qq.com>
2021-08-29 12:43:08 +0800

    添加对不同开发板的适配

--- cmake/board_rm_c.cmake
+++ cmake/board_rm_c.cmake
@@ -1,5 +1,15 @@
+# Board config 
+
+# board specific directory for sources and headers
+set(BOARD_DIR "rm_c")
+
+# Linker script
+set(LINKER_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/ld/STM32F407IGHx_FLASH.ld)
+
+# MCU config
+include(mcu_stm32f4)
+
+# Board definitions 
 add_compile_definitions(
     BOARD_RM_C
 )
-
-set(BOARD_DIR "rm_c")
