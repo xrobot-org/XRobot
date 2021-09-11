c4a7f1680dc7f59a6d43be95fb50a6114a38c63b
Qu Shen <503578404@qq.com>
2021-09-11 22:15:32 +0800

    消息分发添加注释，优化命名

--- src/middleware/mid_msg_distrib.c
+++ src/middleware/mid_msg_distrib.c
@@ -21,14 +21,15 @@
 #include "task.h"
 
 #define MAX_NAME_LEN (20)
-#define MAX_TOPIC (10)
-#define MAX_SUBS_TO_ONE_TPIC (5)
+#define MAX_TOPIC (40)           /* topic上限数量 */
+#define MAX_SUBS_TO_ONE_TPIC (5) /* 每个topic的subscriber上限 */
 
 /* 话题监控 */
 typedef struct {
   uint32_t pub_freq;
-  uint32_t total_pub;
-  uint32_t online_tick;
+  uint32_t num_pub;
+  uint32_t num_suber;
+  uint32_t created_tick;
   uint32_t unexpected;
 } MsgDistrib_TopicMonitor_t;
 
@@ -37,14 +38,13 @@ typedef struct {
   char name[MAX_NAME_LEN];
   void *data_buf;
   size_t data_size;
-  MsgDistrib_Publisher_t pub;
-  MsgDistrib_Subscriber_t subs[MAX_SUBS_TO_ONE_TPIC];
-  uint32_t num_pubs;
-  uint32_t num_subs;
+  MsgDistrib_Publisher_t puber;
+  MsgDistrib_Subscriber_t subers[MAX_SUBS_TO_ONE_TPIC];
 
   MsgDistrib_TopicMonitor_t monitor;
 } MsgDistrib_Topic_t;
 
+/* 消息分发 */
 static struct {
   QueueSetHandle_t topic_queue_set;
   MsgDistrib_Topic_t topic_list[MAX_TOPIC];
@@ -59,6 +59,7 @@ static struct {
  * @return false 失败
  */
 bool MsgDistrib_Init(void) {
+  /* 只能初始化一次 */
   if (md.topic_queue_set == NULL) {
     md.topic_queue_set = xQueueCreateSet(MAX_SUBS_TO_ONE_TPIC);
     md.topic_mutex = xSemaphoreCreateRecursiveMutex();
@@ -78,11 +79,14 @@ bool MsgDistrib_Init(void) {
 MsgDistrib_Publisher_t *MsgDistrib_CreateTopic(const char *topic_name,
                                                size_t data_size) {
   ASSERT(topic_name);
+
+  /* 等待组件初始化 */
   while (md.topic_mutex == NULL) {
     vTaskDelay(pdMS_TO_TICKS(1));
   }
 
-  MsgDistrib_Publisher_t *pub = NULL;
+  MsgDistrib_Publisher_t *puber = NULL;
+
   BaseType_t rtn = xSemaphoreTakeRecursive(md.topic_mutex, portMAX_DELAY);
   if (rtn == pdPASS) {
     if (md.topic_created < MAX_TOPIC) {
@@ -92,19 +96,19 @@ MsgDistrib_Publisher_t *MsgDistrib_CreateTopic(const char *topic_name,
       topic->data_size = data_size;
       topic->data_buf = pvPortMalloc(data_size);
 
-      topic->pub.data_queue = xQueueCreate(1, data_size);
-      xQueueAddToSet(topic->pub.data_queue, md.topic_queue_set);
+      topic->puber.data_queue = xQueueCreate(1, data_size);
+      xQueueAddToSet(topic->puber.data_queue, md.topic_queue_set);
 
-      topic->monitor.online_tick = xTaskGetTickCount();
+      topic->monitor.created_tick = xTaskGetTickCount();
 
       md.topic_created++;
-      pub = &(topic->pub);
+      puber = &(topic->puber);
     } else {
-      pub = NULL;
+      puber = NULL;
     }
     xSemaphoreGiveRecursive(md.topic_mutex);
   }
-  return pub;
+  return puber;
 }
 
 /**
@@ -119,9 +123,10 @@ bool MsgDistrib_Publish(MsgDistrib_Publisher_t *publisher, void *data) {
   ASSERT(publisher);
   ASSERT(data);
 
-  MsgDistrib_Topic_t *topic = CONTAINER_OF(publisher, MsgDistrib_Topic_t, pub);
+  MsgDistrib_Topic_t *topic =
+      CONTAINER_OF(publisher, MsgDistrib_Topic_t, puber);
 
-  topic->monitor.total_pub++;
+  topic->monitor.num_pub++;
   return (xQueueOverwrite(publisher->data_queue, data) == pdPASS);
 }
 
@@ -160,10 +165,11 @@ MsgDistrib_Subscriber_t *MsgDistrib_Subscribe(const char *topic_name,
       MsgDistrib_Topic_t *topic = md.topic_list + i;
       if (strncmp(topic->name, topic_name, MAX_NAME_LEN) == 0) {
         for (size_t j = 0; j < MAX_SUBS_TO_ONE_TPIC; j++) {
-          if (topic->subs[j].bin_sem == NULL) {
-            topic->subs[j].bin_sem = xSemaphoreCreateBinary();
-            topic->subs[j].topic = topic;
-            return topic->subs + j;
+          if (topic->subers[j].bin_sem == NULL) {
+            topic->subers[j].bin_sem = xSemaphoreCreateBinary();
+            topic->subers[j].topic = topic;
+            topic->monitor.num_suber++;
+            return topic->subers + j;
           }
         }
         return NULL;
@@ -214,25 +220,25 @@ void MsgDistrib_Distribute(void) {
     MsgDistrib_Topic_t *topic = md.topic_list + i;
 
     /* 确认话题发布者 */
-    if (topic->pub.data_queue == actived) {
+    if (topic->puber.data_queue == actived) {
       MsgDistrib_TopicMonitor_t *monitor = &(topic->monitor);
       BaseType_t ret;
 
       /* 接收发布者的数据 */
-      ret = xQueueReceive(topic->pub.data_queue, topic->data_buf, 0);
+      ret = xQueueReceive(topic->puber.data_queue, topic->data_buf, 0);
       if (ret != pdPASS) monitor->unexpected++;
 
       /* 分发给订阅者 */
       for (size_t j = 0; j < MAX_SUBS_TO_ONE_TPIC; j++) {
-        if (topic->subs[j].bin_sem != NULL) {
-          xSemaphoreGive(topic->subs[j].bin_sem);
+        if (topic->subers[j].bin_sem != NULL) {
+          xSemaphoreGive(topic->subers[j].bin_sem);
         }
       }
 
       /* 监控话题运行 */
-      if (monitor->total_pub) {
+      if (monitor->num_pub) {
         monitor->pub_freq =
-            (xTaskGetTickCount() - monitor->online_tick) / monitor->total_pub;
+            (xTaskGetTickCount() - monitor->created_tick) / monitor->num_pub;
       } else {
         monitor->pub_freq = 0;
       }
@@ -240,8 +246,21 @@ void MsgDistrib_Distribute(void) {
   }
 }
 
+// TODO: Move to CLI
+#include <stdio.h>
 void MsgDistrib_Detail(char *detail_string, size_t len) {
-  static const char *const header = "\r\n";
-  strncpy(detail_string, header, len - 1);
+  snprintf(detail_string, len - 1, "num topics: %d", md.topic_created);
+
+  static const char *const topic_header =
+      "name                 #pub  pub_freq  #suber  data_size  created at\r\n";
+
+  strncpy(detail_string, topic_header, len - 1);
+
+  snprintf(detail_string, len - 1, " %20s %4ld  %8ld  %6ld  %9d  %10ld",
+           md.topic_list[0].name, md.topic_list[0].monitor.num_pub,
+           md.topic_list[0].monitor.pub_freq,
+           md.topic_list[0].monitor.num_suber, md.topic_list[0].data_size,
+           md.topic_list[0].monitor.created_tick);
+
   detail_string[len] = '\0';
 }
