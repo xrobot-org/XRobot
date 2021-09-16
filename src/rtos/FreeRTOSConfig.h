b49b6a885f6ecbe154b4fb8c44f52a63614c998d
Qu Shen <503578404@qq.com>
2021-09-16 23:18:23 +0800

    将 CCM RAM 用作 RTOS heap

--- src/rtos/FreeRTOSConfig.h
+++ src/rtos/FreeRTOSConfig.h
@@ -74,7 +74,7 @@ extern unsigned long getRunTimeCounterValue(void);
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMAX_PRIORITIES (56)
 #define configMINIMAL_STACK_SIZE ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE ((size_t)0x6000)
+#define configTOTAL_HEAP_SIZE ((size_t)0xF000)
 #define configMAX_TASK_NAME_LEN (16)
 #define configGENERATE_RUN_TIME_STATS 1
 #define configUSE_TRACE_FACILITY 1
