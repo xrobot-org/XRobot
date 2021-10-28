3329a02b81dc007e989f979fd47f4d4d8f31950f
Qu Shen <503578404@qq.com>
2021-10-29 00:41:06 +0800

    根据规则添加模块占位

---
+++ src/module/mod_ore_picker.c
@@ -0,0 +1,10 @@
+/**
+ * @file mod_ore_picker.c
+ * @author Qu Shen
+ * @brief 矿石采集器模块
+ * @version 0.1
+ * @date 2021-10-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
---
+++ src/module/mod_ore_rotater.c
@@ -0,0 +1,10 @@
+/**
+ * @file mod_ore_rotater.c
+ * @author Qu Shen
+ * @brief 矿石旋转器模块
+ * @version 0.1
+ * @date 2021-10-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
