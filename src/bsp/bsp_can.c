9cb62caea4a28e2b9b9610a51eee3a42951665a6
Qu Shen <503578404@qq.com>
2021-10-30 11:01:49 +0800

    代码风格整理

--- src/bsp/bsp_can.c
+++ src/bsp/bsp_can.c
@@ -13,7 +13,6 @@ static BSP_CAN_t CAN_Get(CAN_HandleTypeDef *hcan) {
     return BSP_CAN_ERR;
 }
 
-
 static void BSP_CAN_Callback(BSP_CAN_Callback_t cb_type,
                              CAN_HandleTypeDef *hcan) {
   BSP_CAN_t bsp_can = CAN_Get(hcan);
@@ -27,55 +26,55 @@ static void BSP_CAN_Callback(BSP_CAN_Callback_t cb_type,
 }
 
 void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX0_CPLT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX0_CPLT_CB, hcan);
 }
 
 void HAL_CAN_TxMailbox1CompleteCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX1_CPLT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX1_CPLT_CB, hcan);
 }
 
 void HAL_CAN_TxMailbox2CompleteCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX2_CPLT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX2_CPLT_CB, hcan);
 }
 
 void HAL_CAN_TxMailbox0AbortCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX0_ABORT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX0_ABORT_CB, hcan);
 }
 
 void HAL_CAN_TxMailbox1AbortCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX1_ABORT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX1_ABORT_CB, hcan);
 }
 
 void HAL_CAN_TxMailbox2AbortCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_TX_MAILBOX2_ABORT_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_TX_MAILBOX2_ABORT_CB, hcan);
 }
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_RX_FIFO0_MSG_PENDING_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_RX_FIFO0_MSG_PENDING_CB, hcan);
 }
 
 void HAL_CAN_RxFifo0FullCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_RX_FIFO0_FULL_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_RX_FIFO0_FULL_CB, hcan);
 }
 
 void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_RX_FIFO1_MSG_PENDING_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_RX_FIFO1_MSG_PENDING_CB, hcan);
 }
 
 void HAL_CAN_RxFifo1FullCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_RX_FIFO1_FULL_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_RX_FIFO1_FULL_CB, hcan);
 }
 
 void HAL_CAN_SleepCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_SLEEP_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_SLEEP_CB, hcan);
 }
 
 void HAL_CAN_WakeUpFromRxMsgCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_WAKEUP_FROM_RX_MSG_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_WAKEUP_FROM_RX_MSG_CB, hcan);
 }
 
 void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan) {
-      BSP_CAN_Callback(HAL_CAN_ERROR_CB, hcan);
+  BSP_CAN_Callback(HAL_CAN_ERROR_CB, hcan);
 }
 
 CAN_HandleTypeDef *BSP_CAN_GetHandle(BSP_CAN_t can) {
