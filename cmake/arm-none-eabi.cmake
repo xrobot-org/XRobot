a7210b198d5075accdd2d428504c71b0857b3dc6
Qu Shen <503578404@qq.com>
2021-11-14 13:06:41 +0800

    调整cmake flag位置

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -17,15 +17,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # this makes the test compiles use static library option so that we don't need to pre-set linker flags and scripts
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# General Compiler Options
-
-# Options for DEBUG build
-set(CMAKE_C_FLAGS_DEBUG "-Og -g3 -ggdb3")
-set(CMAKE_ASM_FLAGS_DEBUG "-g3 -ggdb3")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
-
-# Options for RELEASE build
-set(CMAKE_C_FLAGS_RELEASE "-Os")
-set(CMAKE_ASM_FLAGS_RELEASE "")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
