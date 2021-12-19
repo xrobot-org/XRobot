4acea348bc810c559e32a754cffd0ef4c5d82af4
Jiu-xiao <2592509183@qq.com>
2021-12-19 16:05:02 +0800

    修复线程初始化

--- src/thread/thd.c
+++ src/thread/thd.c
@@ -27,20 +27,23 @@
 /* 机器人运行时的数据 */
 runtime_t runtime;
 
-extern const thd_t *_sthread, *_ethread;
+extern const thd_t _sthread, _ethread;
 
 static TaskHandle_t *thd_list;
 
 void thd_init(void) {
   config_get(&runtime.cfg); /* 获取机器人配置 */
 
-  const size_t num_thread = (_ethread - _sthread) / sizeof(thd_t);
+  const thd_t *start_addr = &_sthread;
+  const thd_t *end_addr = &_ethread;
+
+  const size_t num_thread = end_addr - start_addr;
   thd_list = pvPortMalloc(num_thread * sizeof(TaskHandle_t));
 
   /* 创建线程 */
   vTaskSuspendAll();
   for (size_t j = 0; j < num_thread; j++) {
-    const thd_t *thd = _sthread + j;
+    const thd_t *thd = start_addr + j;
     if (thd) {
       xTaskCreate(thd->fn, thd->name, thd->stack_depth, &runtime, thd->priority,
                   thd_list + j);
