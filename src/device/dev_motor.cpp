89bce42b4939c680db1cf337393d91f8b1ee801a
jiu-xiao <2592509183@qq.com>
2022-07-08 22:14:14 +0800

    重构电机收发逻辑

--- src/device/dev_motor.cpp
+++ src/device/dev_motor.cpp
@@ -1,7 +1,5 @@
 #include "dev_motor.hpp"
 
-#include <stdbool.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "comp_utils.hpp"
@@ -19,34 +17,80 @@
 #define MOTOR_ENC_RES (8192)  /* 电机编码器分辨率 */
 #define MOTOR_CUR_RES (16384) /* 电机转矩电流分辨率 */
 
-static bool inited = false;
+using namespace Device;
 
-static const uint32_t MOTOR_CTRL_ID_MAP[] = {M3508_M2006_CTRL_ID_BASE,
-                                             M3508_M2006_CTRL_ID_EXTAND,
-                                             GM6020_CTRL_ID_EXTAND};
+uint8_t Motor::motor_tx_buff_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER][CAN_DATA_SIZE];
 
-static const char *MOTOR_GROUP_NAME_MAP[] = {
-    "chassis", "gimbal_yaw", "gimbal_pit", "launcher_fric", "launcher_trig"};
+uint8_t Motor::motor_tx_flag_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER];
+uint8_t Motor::motor_tx_map_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER];
 
-#define MOTOR_CTRL_ID_NUMBER (sizeof(MOTOR_CTRL_ID_MAP) / sizeof(uint32_t))
+Motor::Motor(const Param &param, const char *name)
+    : param_(param), recv_(sizeof(can_rx_item_t), 1) {
+  strncpy(this->name_, name, sizeof(this->name_));
 
-static uint8_t motor_tx_buff[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER][CAN_DATA_SIZE];
-static bool motor_tx_map[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER];
+  memset(&(this->feedback_), 0, sizeof(this->feedback_));
 
-//   M3508_M1_ID = 0x201, /* 1 */
-//   M3508_M2_ID = 0x202, /* 2 */
-//   M3508_M3_ID = 0x203, /* 3 */
-//   M3508_M4_ID = 0x204, /* 4 */
+  switch (param.id_control) {
+    case M3508_M2006_CTRL_ID_BASE:
+      this->index_ = 0;
+      break;
+    case M3508_M2006_CTRL_ID_EXTAND:
+      this->index_ = 1;
+      break;
+    case GM6020_CTRL_ID_EXTAND:
+      this->index_ = 2;
+      break;
+    default:
+      ASSERT(false);
+  }
 
-//   M3508_FRIC1_ID = 0x205, /* 5 */
-//   M3508_FRIC2_ID = 0x206, /* 6 */
-//   M2006_TRIG_ID = 0x207,  /* 7 */
+  om_user_fun_t rx_callback = [](om_msg_t *msg, void *arg) {
+    can_rx_item_t *rx = (can_rx_item_t *)msg->buff;
 
-//   GM6020_YAW_ID = 0x209, /* 5 */
-//   GM6020_PIT_ID = 0x20A, /* 6 */
+    Motor *motor = (Motor *)arg;
 
-static float motor_model_to_lsb(motor_model_t model) {
-  switch (model) {
+    motor->recv_.OverwriteFromISR(rx);
+
+    return OM_OK;
+  };
+
+  System::Message::Topic<can_rx_item_t> motor_tp(this->name_, false);
+
+  System::Message::Subscription<can_rx_item_t> motor_sub(motor_tp, rx_callback,
+                                                         this);
+
+  bsp_can_register_subscriber(this->param_.can, motor_tp.GetHandle(),
+                              this->param_.id_feedback, 1);
+
+  motor_tx_map_[this->param_.can][this->index_] |= 1 << (this->param_.num);
+}
+
+bool Motor::Update() {
+  can_rx_item_t pack;
+
+  while (this->recv_.Receive(&pack, 0)) {
+    if ((pack.index == this->param_.id_feedback) &&
+        (MOTOR_NONE != this->param_.model)) {
+      this->Decode(pack);
+    }
+  }
+
+  return true;
+}
+
+void Motor::Decode(can_rx_item_t &rx) {
+  uint16_t raw_angle = (uint16_t)((rx.data[0] << 8) | rx.data[1]);
+  int16_t raw_current = (int16_t)((rx.data[4] << 8) | rx.data[5]);
+
+  this->feedback_.rotor_abs_angle = raw_angle / (float)MOTOR_ENC_RES * M_2PI;
+  this->feedback_.rotational_speed = (int16_t)((rx.data[2] << 8) | rx.data[3]);
+  this->feedback_.torque_current =
+      raw_current * M3508_MAX_ABS_CUR / (float)MOTOR_CUR_RES;
+  this->feedback_.temp = rx.data[6];
+}
+
+float Motor::GetLSB() {
+  switch (this->param_.model) {
     case MOTOR_M2006:
       return (float)M2006_MAX_ABS_LSB;
 
@@ -57,162 +101,46 @@ static float motor_model_to_lsb(motor_model_t model) {
       return (float)GM6020_MAX_ABS_LSB;
 
     default:
-      return 0.f;
+      return 0.0f;
   }
 }
 
-static uint32_t motor_get_tx_data_offset(motor_model_t model,
-                                         uint32_t feedback_id,
-                                         uint32_t control_id) {
-  switch (model) {
-    case MOTOR_M3508:
-    case MOTOR_M2006:
-      switch (control_id) {
-        case M3508_M2006_CTRL_ID_BASE:
-          return feedback_id - M3508_M2006_FB_ID_BASE;
+bool Motor::AddData() {
+  float lsb = this->GetLSB();
 
-        case M3508_M2006_CTRL_ID_EXTAND:
-          return feedback_id - M3508_M2006_FB_ID_EXTAND;
+  if (lsb != 0.0f) {
+    int16_t ctrl_cmd = this->output_ * lsb;
+    motor_tx_buff_[this->param_.can][this->index_][2 * this->param_.num] =
+        (uint8_t)((ctrl_cmd >> 8) & 0xFF);
+    motor_tx_buff_[this->param_.can][this->index_][2 * this->param_.num + 1] =
+        (uint8_t)(ctrl_cmd & 0xFF);
+    motor_tx_flag_[this->param_.can][this->index_] |= 1 << (this->param_.num);
 
-        default:
-          return 0;
-      }
-    case MOTOR_GM6020:
-      switch (control_id) {
-        case GM6020_CTRL_ID_BASE:
-          return feedback_id - GM6020_FB_ID_BASE;
-        case GM6020_CTRL_ID_EXTAND:
-          return feedback_id - GM6020_FB_ID_EXTAND;
-
-        default:
-          return 0;
-      }
-    default:
-      return 0;
-  }
-}
-
-static int8_t motor_decode(motor_feedback_t *fb, const uint8_t *raw) {
-  ASSERT(fb);
-  ASSERT(raw);
-
-  uint16_t raw_angle = (uint16_t)((raw[0] << 8) | raw[1]);
-  int16_t raw_current = (int16_t)((raw[4] << 8) | raw[5]);
-
-  fb->rotor_abs_angle = raw_angle / (float)MOTOR_ENC_RES * M_2PI;
-  fb->rotational_speed = (int16_t)((raw[2] << 8) | raw[3]);
-  fb->torque_current = raw_current * M3508_MAX_ABS_CUR / (float)MOTOR_CUR_RES;
-  fb->temp = raw[6];
-  return RM_OK;
-}
-
-om_status_t motor_rx_callback(om_msg_t *msg, void *arg) {
-  ASSERT(msg);
-  ASSERT(arg);
-  QueueHandle_t msgq = (QueueHandle_t)arg;
-  can_rx_item_t *rx = (can_rx_item_t *)msg->buff;
-
-  BaseType_t switch_required;
-  xQueueSendFromISR(msgq, rx, &switch_required);
-  portYIELD_FROM_ISR(switch_required);
-
-  return OM_OK;
-}
-
-int8_t motor_init(motor_t *motor, const motor_group_t *group_cfg) {
-  ASSERT(motor);
-  ASSERT(group_cfg);
-  ASSERT(sizeof(MOTOR_GROUP_NAME_MAP) / sizeof(char *) == MOTOR_GROUP_ID_NUM);
-
-  bsp_can_wait_init();
-
-  if (inited) return DEVICE_ERR_INITED;
-  motor->group_cfg = group_cfg;
-
-  char tp_name[OM_TOPIC_MAX_NAME_LEN] = {0};
-
-  const motor_group_t *group = motor->group_cfg;
-  for (int i = 0; i < MOTOR_GROUP_ID_NUM; i++) {
-    snprintf(tp_name, OM_LOG_MAX_LEN, "can_motor_%s", MOTOR_GROUP_NAME_MAP[i]);
-    motor->msgq[i] = xQueueCreate(8, sizeof(can_rx_item_t));
-    om_topic_t *motor_tp = om_config_topic(NULL, "DVA", tp_name,
-                                           motor_rx_callback, motor->msgq[i]);
-
-    bsp_can_register_subscriber(group->can, motor_tp, group->id_feedback,
-                                group->num);
-
-    group++;
-  }
-
-  inited = true;
-  return DEVICE_OK;
-}
-
-int8_t motor_update(motor_t *motor) {
-  can_rx_item_t pack;
-
-  for (int i = 0; i < MOTOR_GROUP_ID_NUM; i++) {
-    while (pdPASS == xQueueReceive(motor->msgq[i], &pack, 0)) {
-      uint32_t index = pack.index - motor->group_cfg[i].id_feedback;
-      if ((index < motor->group_cfg[i].num) &&
-          (MOTOR_NONE != motor->group_cfg[i].model[index])) {
-        motor_decode(&(motor->feedback[i].as_array[index]), pack.data);
-      }
+    if (((~motor_tx_flag_[this->param_.can][this->index_]) &
+         (motor_tx_map_[this->param_.can][this->index_])) == 0) {
+      this->SendData();
     }
+    return true;
   }
 
-  return RM_OK;
+  return false;
 }
 
-int8_t motor_pack_data(motor_t *motor, motor_group_id_t group,
-                      motor_control_t *output) {
-  ASSERT(output);
-  ASSERT(motor);
+bool Motor::SendData() {
+  bsp_can_trans_packet(this->param_.can, this->param_.id_control,
+                       motor_tx_buff_[this->param_.can][this->index_],
+                       &this->mailbox_, 1);
 
-  uint8_t *data = NULL;
+  motor_tx_flag_[this->param_.can][this->index_] = 0;
 
-  for (uint32_t i = 0; i < MOTOR_CTRL_ID_NUMBER; i++) {
-    if (motor->group_cfg[group].id_control == MOTOR_CTRL_ID_MAP[i]) {
-      motor_tx_map[motor->group_cfg[group].can][i] = true;
-      data = motor_tx_buff[motor->group_cfg[group].can][i];
-      break;
-    }
-  }
-
-  int16_t motor_data;
-
-  uint32_t id_offset = motor_get_tx_data_offset(
-      *motor->group_cfg[group].model, motor->group_cfg[group].id_feedback,
-      motor->group_cfg[group].id_control);
-
-  for (size_t i = 0; i < motor->group_cfg[group].num; i++) {
-    float lsb = motor_model_to_lsb(motor->group_cfg[group].model[i]);
-
-    if (lsb) {
-      motor_data = (int16_t)(output->as_array[i] * lsb);
-      data[2 * (i + id_offset)] = (uint8_t)((motor_data >> 8) & 0xFF);
-      data[2 * (i + id_offset) + 1] = (uint8_t)(motor_data & 0xFF);
-    }
-  }
-  return RM_OK;
+  return true;
 }
 
-int8_t motor_control(motor_t *motor) {
-  for (uint32_t i = 0; i < BSP_CAN_NUM; i++) {
-    for (uint32_t t = 0; t < MOTOR_CTRL_ID_NUMBER; t++) {
-      if (motor_tx_map[i][t])
-        bsp_can_trans_packet((bsp_can_t)i, MOTOR_CTRL_ID_MAP[t],
-                             motor_tx_buff[i][t], &motor->mailbox, 1);
-    }
-  }
-
-  memset(&motor_tx_map, 0, sizeof(motor_tx_map));
-
-  return RM_OK;
+void Motor::Control(float out) {
+  clampf(&out, -1.0f, 1.0f);
+  this->output_ = out;
 }
 
-int8_t motor_handle_offline(motor_t *motor) {
-  RM_UNUSED(motor);
-  memset(motor, 0, sizeof(*motor));
-  return RM_OK;
+void Motor::Offline() {
+  memset(&(this->feedback_), 0, sizeof(this->feedback_));
 }
--- src/device/dev_motor.hpp
+++ src/device/dev_motor.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <stdbool.h>
+#include <string>
 
-#include "FreeRTOS.h"
 #include "bsp_can.h"
 #include "comp_ahrs.hpp"
 #include "comp_utils.hpp"
 #include "dev.hpp"
-#include "queue.h"
 
 /* Motor id */
 /* id     feedback id     control id */
@@ -27,105 +25,69 @@
 #define M3508_M2006_CTRL_ID_EXTAND (0x1ff)
 #define M3508_M2006_ID_SETTING_ID (0x700)
 
-/* 电机型号 */
-typedef enum {
-  MOTOR_NONE = 0,
-  MOTOR_M2006,
-  MOTOR_M3508,
-  MOTOR_GM6020,
-} motor_model_t;
+#define MOTOR_CTRL_ID_NUMBER (3)
 
-typedef struct {
-  uint32_t id_feedback;
-  uint32_t id_control;
-  motor_model_t model[4];
-  uint8_t num;
-  bsp_can_t can;
-} motor_group_t;
+namespace Device {
+class Motor {
+ public:
+  typedef enum {
+    MOTOR_NONE = 0,
+    MOTOR_M2006,
+    MOTOR_M3508,
+    MOTOR_GM6020,
+  } Model;
 
-typedef union {
-  float as_array[4];
+  typedef struct {
+    uint32_t id_feedback;
+    uint32_t id_control;
+    Model model;
+    uint8_t num;
+    bsp_can_t can;
+  } Param;
 
-  struct {
-    float m1;
-    float m2;
-    float m3;
-    float m4;
-  } as_chassis;
+  typedef struct {
+    float rotor_abs_angle;  /* 转子绝对角度 单位：rad */
+    float rotational_speed; /* 转速 单位：rpm */
+    float torque_current;   /* 转矩电流 单位：A*/
+    float temp;             /* 电机温度 单位：℃*/
+  } Feedback;
 
-  struct {
-    float yaw;
-  } as_gimbal_yaw;
+  Motor(const Param& param, const char* name);
 
-  struct {
-    float pit;
-  } as_gimbal_pit;
+  Motor(Motor& motor);
 
-  struct {
-    float fric_left;
-    float fric_right;
-  } as_launcher_fric;
+  void Decode(can_rx_item_t& rx);
 
-  struct {
-    float trig;
-  } as_launcher_trig;
-} motor_control_t;
+  bool Update();
 
-/* 电机反馈信息 */
-typedef struct {
-  float rotor_abs_angle;  /* 转子绝对角度 单位：rad */
-  float rotational_speed; /* 转速 单位：rpm */
-  float torque_current;   /* 转矩电流 单位：A*/
-  float temp;             /* 电机温度 单位：℃*/
-} motor_feedback_t;
+  bool AddData();
 
-typedef union {
-  motor_feedback_t as_array[4];
+  bool SendData();
 
-  struct {
-    motor_feedback_t m1;
-    motor_feedback_t m2;
-    motor_feedback_t m3;
-    motor_feedback_t m4;
-  } as_chassis;
+  void Control(float out);
 
-  struct {
-    motor_feedback_t yaw;
-  } as_gimbal_yaw;
+  void Offline();
 
-  struct {
-    motor_feedback_t pit;
-  } as_gimbal_pit;
+  float GetLSB();
 
-  struct {
-    motor_feedback_t fric_left;
-    motor_feedback_t fric_right;
-  } as_launcher_fric;
+  Param param_;
 
-  struct {
-    motor_feedback_t trig;
-  } as_launcher_trig;
-} motor_feedback_group_t;
+  Feedback feedback_;
 
-typedef enum {
-  MOTOR_GROUP_ID_CHASSIS = 0,
-  MOTOR_GROUP_ID_GIMBAL_YAW,
-  MOTOR_GROUP_ID_GIMBAL_PIT,
-  MOTOR_GROUP_ID_LAUNCHER_FRIC,
-  MOTOR_GROUP_ID_LAUNCHER_TRIG,
-  MOTOR_GROUP_ID_NUM,
-} motor_group_id_t;
+  uint8_t index_;
 
-typedef struct {
-  motor_feedback_group_t feedback[MOTOR_GROUP_ID_NUM];
-  QueueHandle_t msgq[MOTOR_GROUP_ID_NUM];
-  uint32_t mailbox;
-  const motor_group_t *group_cfg;
-} motor_t;
+  uint32_t mailbox_;
 
-int8_t motor_init(motor_t *motor, const motor_group_t *group_cfg);
-int8_t motor_update(motor_t *motor);
-int8_t motor_pack_data(motor_t *motor, motor_group_id_t group,
-                      motor_control_t *output);
-int8_t motor_control(motor_t *motor);
-int8_t motor_handle_offline(motor_t *motor);
+  char name_[20];
+
+  float output_;
+
+  static uint8_t motor_tx_buff_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER]
+                               [CAN_DATA_SIZE];
+
+  static uint8_t motor_tx_flag_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER];
+  static uint8_t motor_tx_map_[BSP_CAN_NUM][MOTOR_CTRL_ID_NUMBER];
+
+  System::Queue recv_;
+};
+}  // namespace Device
