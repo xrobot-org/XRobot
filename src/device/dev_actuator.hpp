8345861d0cd9cf408e146d289b63fc70d40652d2
jiu-xiao <2592509183@qq.com>
2022-08-08 10:52:35 +0800

    执行器与电机解耦

--- src/device/dev_actuator.hpp
+++ src/device/dev_actuator.hpp
@@ -10,91 +10,47 @@
 #include "dev_motor.hpp"
 
 namespace Device {
-class Actuator {
+class SpeedActuator {
  public:
   typedef struct {
     Component::PID::Param speed;
-    Component::PID::Param pos;
     float in_cutoff_freq;
     float out_cutoff_freq;
-    Device::Motor::Param motor;
-    bool reverse;
-  } PosParam;
+  } Param;
 
-  typedef struct {
-    Component::PID::Param speed;
-    float in_cutoff_freq;
-    float out_cutoff_freq;
-    Device::Motor::Param motor;
-    bool reverse;
-  } SpeedParam;
+  SpeedActuator(Param& param, float sample_freq);
 
-  Actuator(PosParam& param, float sample_freq, const char* name);
-
-  Actuator(SpeedParam& param, float sample_freq, const char* name);
-
-  bool Control(float setpoint, float dt);
-
-  bool Control(float setpoint, float pos_fb, float speed_fb, float dt);
-
-  bool UpdateFeedback();
+  float Control(float setpoint, float feedback, float dt);
 
   void Reset();
 
-  void Relax();
+  Component::PID pid_;
 
-  float GetPos();
-
-  float GetSpeed();
-
-  Component::PID *speed_, *pos_;
-
-  Component::LowPassFilter2p in_pos_;
-  Component::LowPassFilter2p in_speed_;
+  Component::LowPassFilter2p in_;
   Component::LowPassFilter2p out_;
-
-  Device::Motor motor_;
-
-  bool reverse_;
 };
 
-class LimitedActuator : public Actuator {
+class PosActuator {
  public:
   typedef struct {
-    float min_k_percent;
-    float max_k_percent;
-    float max_load;
-    float min_current;
-    float filter_k;
-  } LimitParam;
+    Component::PID::Param speed;
+    Component::PID::Param position;
+    float in_cutoff_freq;
+    float out_cutoff_freq;
+  } Param;
 
-  LimitedActuator(SpeedParam& speed_param, LimitParam& limit_param,
-                  float sample_freq, const char* name);
+  PosActuator(Param& param, float sample_freq);
 
-  static bool Control(float* setpoint, LimitedActuator* actuator, uint8_t len,
-                      float limit_rate, float dt);
-
-  bool UpdateFeedback();
+  float Control(float setpoint, float speed_fb, float pos_fb, float dt);
 
   void Reset();
 
-  Component::LowPassFilter in_load_;
+  Component::PID pid_speed_;
+  Component::PID pid_position_;
 
-  LimitParam& limit_param_;
+  Component::LowPassFilter2p in_speed_;
+  Component::LowPassFilter2p in_position_;
 
-  float load_;
-
-  const float default_k_;
-};
-
-class FeedForwardActuator : public Actuator {
- public:
-  FeedForwardActuator(Component::SecOrderFunction::Param& ff_param,
-                      PosParam& pos_param, float sample_freq, const char* name);
-
-  bool Control(float setpoint, float pos_fb, float speed_fb, float ff_fb,
-               float dt);
-
-  Component::SecOrderFunction ff_;
+  Component::LowPassFilter2p out_;
 };
 }  // namespace Device
