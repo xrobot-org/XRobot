6b7b97e2c08054eddbc3e87d08747a391e08dbf9
jiu-xiao <2592509183@qq.com>
2022-08-20 21:48:24 +0800

    支持VSCODE 调试

--- hw/hw.cmake
+++ hw/hw.cmake
@@ -4,9 +4,11 @@ set(BSP_DIR ${HW_DIR}/bsp)
 set(MCU_DIR ${HW_DIR}/mcu)
 
 if(CONFIG_BOARD_DEV_C)
-  set(CMAKE_MODULE_PATH ${BSP_DIR}/rm-c)
+  set(BOARD_DIR ${BSP_DIR}/rm-c)
 else()
   message(FATAL_ERROR "No board selected.")
 endif()
 
+set(CMAKE_MODULE_PATH ${BOARD_DIR})
 include(board)
+execute_process(COMMAND cp ./debug/launch.json ${CMAKE_CURRENT_SOURCE_DIR}/.vscode WORKING_DIRECTORY ${BOARD_DIR})
