78a7018da24852837711df3e0949ad210022e3dc
jiu-xiao <2592509183@qq.com>
2022-09-08 22:56:05 +0800

    ahrs不再区分IMU来源

--- src/component/comp_ahrs.hpp
+++ src/component/comp_ahrs.hpp
@@ -7,14 +7,13 @@
 
 #include "comp_type.hpp"
 #include "comp_utils.hpp"
+#include "message.hpp"
 #include "thread.hpp"
 
 namespace Component {
 class AHRS {
  public:
-  typedef enum { GIMBAL, CHASSIS } Source;
-
-  AHRS(Source source);
+  AHRS();
 
   void Update();
 
@@ -24,12 +23,12 @@ class AHRS {
   float dt_;
   float now_;  // TODO:封装
 
-  Component::Type::Quaternion quat_;
-  Component::Type::Vector3 accl_, gyro_;
-  Component::Type::Eulr eulr_;
-
-  Source source_;
-
   System::Thread thread_;
+
+  DECLARE_PUBER(quat_, Type::Quaternion, "imu_quat", true);
+  DECLARE_PUBER(eulr_, Type::Eulr, "imu_eulr", true);
+
+  Type::Vector3 accl_;
+  Type::Vector3 gyro_;
 };
 }  // namespace Component
