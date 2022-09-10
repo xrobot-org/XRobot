ff87fb1a32ede398129e0c6492235441154ee517
任乃华 <2563730049@qq.com>
2022-09-10 21:37:02 +0800

    can初始化在bsp中完成

--- src/device/dev_can.cpp
+++ src/device/dev_can.cpp
@@ -8,33 +8,4 @@
 
 using namespace Device;
 
-can_rx_item_t raw_rx1;
-can_rx_item_t raw_rx2;
-
-CAN::CAN() {
-  auto can1_callback = [](void *arg) {
-    RM_UNUSED(arg);
-
-    while (bsp_can_get_msg(BSP_CAN_1, &raw_rx1) == BSP_OK) {
-      om_publish(bsp_can_get_topic(BSP_CAN_1), OM_PRASE_VAR(raw_rx1), true,
-                 true);
-    }
-  };
-
-  auto can2_callback = [](void *arg) {
-    RM_UNUSED(arg);
-
-    while (bsp_can_get_msg(BSP_CAN_2, &raw_rx2) == BSP_OK) {
-      om_publish(bsp_can_get_topic(BSP_CAN_2), OM_PRASE_VAR(raw_rx2), true,
-                 true);
-    }
-  };
-
-  bsp_can_register_callback(BSP_CAN_1, HAL_CAN_RX_FIFO0_MSG_PENDING_CB,
-                            can1_callback, NULL);
-
-  bsp_can_register_callback(BSP_CAN_2, HAL_CAN_RX_FIFO1_MSG_PENDING_CB,
-                            can2_callback, NULL);
-
-  bsp_can_init();
-}
+CAN::CAN() { bsp_can_init(); }
