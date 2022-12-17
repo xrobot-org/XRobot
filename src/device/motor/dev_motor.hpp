1d5f3f24277dd30ef7d053a1e7348f0ff3a890eb
xiao <2592509183@qq.com>
2022-12-17 23:20:08 +0800

    电机命令支持连续打印

--- src/device/motor/dev_motor.hpp
+++ src/device/motor/dev_motor.hpp
@@ -33,18 +33,32 @@ class BaseMotor {
   float GetCurrent() { return this->feedback_.torque_current; }
 
   static void ShowCMD(BaseMotor *motor, int argc, char *argv[]) {
-    RM_UNUSED(argc);
-    RM_UNUSED(argv);
+    if (argc == 1) {
+      ms_printf("[show] [time] [delay] 在time时间内每隔delay打印一次数据");
+      ms_enter();
+    } else if (argc == 4) {
+      if (strcmp(argv[1], "show") == 0) {
+        int time = std::stoi(argv[2]);
+        int delay = std::stoi(argv[3]);
 
-    ms_printf("电机 [%s] 反馈数据:", motor->name_);
-    ms_enter();
-    ms_printf("最近一次反馈时间:%fs.", motor->last_online_time_);
-    ms_enter();
-    ms_printf("角度:%frad 速度:%frpm 电流:%fA 温度:%f℃",
-              motor->feedback_.rotor_abs_angle,
-              motor->feedback_.rotational_speed,
-              motor->feedback_.torque_current, motor->feedback_.temp);
-    ms_enter();
+        if (delay > 1000) delay = 1000;
+        if (delay < 2) delay = 2;
+        while (time > delay) {
+          ms_printf("电机 [%s] 反馈数据:", motor->name_);
+          ms_enter();
+          ms_printf("最近一次反馈时间:%fs.", motor->last_online_time_);
+          ms_enter();
+          ms_printf("角度:%frad 速度:%frpm 电流:%fA 温度:%f℃",
+                    motor->feedback_.rotor_abs_angle,
+                    motor->feedback_.rotational_speed,
+                    motor->feedback_.torque_current, motor->feedback_.temp);
+          ms_enter();
+          System::Thread::Sleep(delay);
+          ms_clear_line();
+          time -= delay;
+        }
+      }
+    }
   }
 
   char name_[20];
