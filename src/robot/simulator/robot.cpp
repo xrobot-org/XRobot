e909d56f520639fe578257c5fd2708132163458c
xiao <2592509183@qq.com>
2022-12-17 23:35:49 +0800

    添加仿真机器人类型

--- src/robot/simulator/robot.cpp
+++ src/robot/simulator/robot.cpp
@@ -3,14 +3,128 @@
 #include "thread.hpp"
 
 /* clang-format off */
+Robot::Simulator::Param param = {
+  .led = {
+    .gpio = BSP_GPIO_LED,
+    .timeout = 200,
+  },
 
+  .chassis={
+      .type = Component::Mixer::MECANUM,
+
+      .follow_pid_param = {
+      .k = 0.5f,
+      .p = 1.0f,
+      .i = 0.0f,
+      .d = 0.0f,
+      .i_limit = 1.0f,
+      .out_limit = 1.0f,
+      .d_cutoff_freq = -1.0f,
+      .range = M_2PI,
+    },
+
+    .event_map = {
+      Component::CMD::CreateMapItem(
+        Device::TerminalController::Stop,
+        Module::RMChassis::ChangeModeRelax
+      ),
+      Component::CMD::CreateMapItem(
+        Device::TerminalController::Start,
+        Module::RMChassis::ChangeModeFollow
+      ),
+    },
+
+    .actuator_param = {
+      {
+        .speed = {
+          .k = 0.003f,
+          .p = 1.0f,
+          .i = 0.5f,
+          .d = 0.0f,
+          .i_limit = 0.02f,
+          .out_limit = 1.0f,
+          .d_cutoff_freq = -1.0f,
+          .range = -1.0f,
+        },
+
+        .in_cutoff_freq = -1.0f,
+
+        .out_cutoff_freq = -1.0f,
+
+      },
+      {
+        .speed = {
+          .k = 0.003f,
+          .p = 1.0f,
+          .i = 0.5f,
+          .d = 0.0f,
+          .i_limit = 0.02f,
+          .out_limit = 1.0f,
+          .d_cutoff_freq = -1.0f,
+          .range = -1.0f,
+        },
+
+        .in_cutoff_freq = -1.0f,
+
+        .out_cutoff_freq = -1.0f,
+      },
+      {
+        .speed = {
+          .k = 0.003f,
+          .p = 1.0f,
+          .i = 0.5f,
+          .d = 0.0f,
+          .i_limit = 0.02f,
+          .out_limit = 1.0f,
+          .d_cutoff_freq = -1.0f,
+          .range = -1.0f,
+        },
+
+        .in_cutoff_freq = -1.0f,
+
+        .out_cutoff_freq = -1.0f,
+      },
+      {
+        .speed = {
+          .k = 0.003f,
+          .p = 1.0f,
+          .i = 0.5f,
+          .d = 0.0f,
+          .i_limit = 0.02f,
+          .out_limit = 1.0f,
+          .d_cutoff_freq = -1.0f,
+          .range = -1.0f,
+        },
+
+        .in_cutoff_freq = -1.0f,
+
+        .out_cutoff_freq = -1.0f,
+      },
+    },
+
+    .motor_param = {
+      {
+          .model = Device::RMMotor::MOTOR_M3508,
+      },
+      {
+          .model = Device::RMMotor::MOTOR_M3508,
+      },
+      {
+          .model = Device::RMMotor::MOTOR_M3508,
+      },
+      {
+          .model = Device::RMMotor::MOTOR_M3508,
+      },
+    },
+  },
+};
 /* clang-format on */
 
 void robot_init() {
   auto init_thread_fn = [](void* arg) {
     RM_UNUSED(arg);
 
-    Robot::Simulator blink;
+    Robot::Simulator blink(param);
 
     while (1) {
       System::Thread::Sleep(UINT32_MAX);
--- src/robot/simulator/robot.hpp
+++ src/robot/simulator/robot.hpp
@@ -1,4 +1,7 @@
 #include "comp_utils.hpp"
+#include "dev_blink_led.hpp"
+#include "dev_controller.hpp"
+#include "mod_chassis.hpp"
 #include "om.hpp"
 #include "term.hpp"
 
@@ -7,10 +10,26 @@ void robot_init();
 namespace Robot {
 class Simulator {
  public:
+  typedef struct {
+    Device::BlinkLED::Param led;
+    Module::RMChassis::Param chassis;
+  } Param;
+
   Message message_;
 
   System::Term term_;
 
-  Simulator() {}
+  Component::CMD cmd_;
+
+  Device::BlinkLED led_;
+  Device::Referee referee_;
+  Device::TerminalController ctrl_;
+
+  Module::RMChassis chassis_;
+
+  Simulator(Param& param)
+      : cmd_(Component::CMD::TerminalControl),
+        led_(param.led),
+        chassis_(param.chassis, 500) {}
 };
 }  // namespace Robot
