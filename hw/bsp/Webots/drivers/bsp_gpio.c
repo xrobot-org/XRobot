635fb1882cec7bc212f700024c59b8dfe21161c3
xiao <2592509183@qq.com>
2022-12-19 20:36:00 +0800

    仿真添加LED显示

--- hw/bsp/Webots/drivers/bsp_gpio.c
+++ hw/bsp/Webots/drivers/bsp_gpio.c
@@ -2,11 +2,23 @@
 
 #include <stdlib.h>
 
+#include "webots/led.h"
+
+static WbDeviceTag led = 0;
+
 inline int8_t bsp_gpio_write_pin(bsp_gpio_t gpio, bool value) {
   (void)gpio;
 
+  if (!led) {
+    led = wb_robot_get_device("led");
+  }
+
   if (value) {
-    if (!system("spd-say -r 100 -p 100 -R 100  滴")) return BSP_OK;
+    wb_led_set(led, 1);
+    return BSP_OK;
+  } else {
+    wb_led_set(led, 0);
+    return BSP_OK;
   }
 
   return BSP_ERR;
