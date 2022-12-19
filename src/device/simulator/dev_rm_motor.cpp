bcc89d7675aef51027f81f27a6a9829c58fe4e7c
xiao <2592509183@qq.com>
2022-12-19 20:38:47 +0800

    优化电机反馈速度计算方式

--- src/device/simulator/dev_rm_motor.cpp
+++ src/device/simulator/dev_rm_motor.cpp
@@ -33,18 +33,20 @@ void RMMotor::Control(float output) {
 }
 
 bool RMMotor::Update() {
-  float pos = wb_position_sensor_get_value(this->sensor_);
-
-  while (pos < 0.0f) pos += M_2PI;
-  while (pos > M_2PI) pos -= M_2PI;
+  float raw_pos = wb_position_sensor_get_value(this->sensor_);
 
   this->feedback_.rotational_speed =
-      circle_error(pos, this->last_pos_, M_2PI) /
+      circle_error(raw_pos, this->last_pos_, M_2PI) /
       (bsp_time_get() - this->last_sensor_time_) / M_2PI * 60.0f * 19.0f;
 
-  this->last_pos_ = this->feedback_.rotor_abs_angle;
+  this->last_pos_ = raw_pos;
 
-  this->feedback_.rotor_abs_angle = pos;
+  raw_pos *= 19.0;
+
+  while (raw_pos < 0.0f) raw_pos += M_2PI;
+  while (raw_pos > M_2PI) raw_pos -= M_2PI;
+
+  this->feedback_.rotor_abs_angle = raw_pos;
 
   this->feedback_.temp = 28.0f;
 
