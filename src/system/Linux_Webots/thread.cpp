bf418e27a1cc99565baf1bbfae305a1e8c94ea0f
xiao <2592509183@qq.com>
2023-01-13 23:51:50 +0800

    修复模拟器延时精度太低的问题

--- src/system/Linux_Webots/thread.cpp
+++ src/system/Linux_Webots/thread.cpp
@@ -6,13 +6,9 @@
 
 using namespace System;
 
-static bool simulating = false;
-
 void Thread::StartKernel() {
   while (1) {
-    simulating = true;
     wb_robot_step(1);
-    simulating = false;
     poll(NULL, 0, 2);
   }
 }
@@ -20,7 +16,7 @@ void Thread::StartKernel() {
 void Thread::Sleep(uint32_t microseconds) {
   float time = bsp_time_get() + microseconds / 1000.0f;
 
-  while (simulating || bsp_time_get() < time) {
+  while (bsp_time_get() < time) {
     poll(NULL, 0, 1);
   }
 }
@@ -28,7 +24,7 @@ void Thread::Sleep(uint32_t microseconds) {
 void Thread::SleepUntil(uint32_t microseconds) {
   float time = bsp_time_get() + microseconds / 1000.0f;
 
-  while (simulating || bsp_time_get() < time) {
+  while (bsp_time_get() < time) {
     poll(NULL, 0, 1);
   }
 }
