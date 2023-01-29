bc62933ae9725aeb67ee62d4a42553d9e6c96553
xiao <2592509183@qq.com>
2023-01-29 12:44:19 +0800

    完善database api

--- src/system/Linux/database.hpp
+++ src/system/Linux/database.hpp
@@ -33,7 +33,29 @@ class Database {
       }
     }
 
+    Key(const char* name, const Data& init_value) : name_(name) {
+      FILE* fd = NULL;
+      if (!access((path_ + name).c_str(), W_OK)) {
+        fd = fopen((path_ + name).c_str(), "r");
+        static_cast<void>(fread(&this->data_, sizeof(Data), 1, fd));
+        static_cast<void>(fclose(fd));
+      } else {
+        fd = fopen((path_ + name).c_str(), "w+");
+        this->data_ = init_value;
+        static_cast<void>(fwrite(&this->data_, sizeof(Data), 1, fd));
+        static_cast<void>(fclose(fd));
+      }
+    }
+
     void Set() {
+      FILE* fd = NULL;
+      fd = fopen((path_ + name_).c_str(), "w+");
+      static_cast<void>(fwrite(&this->data_, sizeof(Data), 1, fd));
+      static_cast<void>(fclose(fd));
+    }
+
+    void Set(const Data& data) {
+      this->data_ = data;
       FILE* fd = NULL;
       fd = fopen((path_ + name_).c_str(), "w+");
       static_cast<void>(fwrite(&this->data_, sizeof(Data), 1, fd));
