20dbe9d34eafa3bb126ddabdfb2951dd48574fba
Ar <1508170637@qq.com>
2023-02-21 17:11:54 +0800

    Add hero robot type.

---
+++ src/robot/hero/robot.hpp
@@ -0,0 +1,48 @@
+#include "comp_cmd.hpp"
+#include "dev_ahrs.hpp"
+#include "dev_ai.hpp"
+#include "dev_bmi088.hpp"
+#include "dev_can.hpp"
+#include "dev_cap.hpp"
+#include "dev_dr16.hpp"
+#include "dev_led_rgb.hpp"
+#include "dev_referee.hpp"
+#include "mod_chassis.hpp"
+#include "mod_gimbal.hpp"
+#include "mod_launcher.hpp"
+
+void robot_init();
+namespace Robot {
+class Hero {
+ public:
+  typedef struct {
+    Module::RMChassis::Param chassis;
+    Module::Gimbal::Param gimbal;
+    Module::Launcher::Param launcher;
+    Device::BMI088::Rotation bmi088_rot;
+    Device::Cap::Param cap;
+  } Param;
+
+  Component::CMD cmd_;
+
+  Device::AI ai_;
+  Device::AHRS ahrs_;
+  Device::BMI088 bmi088_;
+  Device::Can can_;
+  Device::Cap cap_;
+  Device::DR16 dr16_;
+  Device::RGB led_;
+  Device::Referee referee_;
+
+  Module::RMChassis chassis_;
+  Module::Gimbal gimbal_;
+  Module::Launcher launcher_;
+
+  Hero(Param& param, float control_freq)
+      : bmi088_(param.bmi088_rot),
+        cap_(param.cap),
+        chassis_(param.chassis, control_freq),
+        gimbal_(param.gimbal, control_freq),
+        launcher_(param.launcher, control_freq) {}
+};
+}  // namespace Robot
