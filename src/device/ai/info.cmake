444e603419eed686aa6ac81128f6058b561571c0
xiao <2592509183@qq.com>
2023-03-23 23:28:33 +0800

    更新Host通信协议仓库路径

--- src/device/ai/info.cmake
+++ src/device/ai/info.cmake
@@ -2,7 +2,7 @@ CHECK_SUB_ENABLE(MODULE_ENABLE device)
 
 if(${MODULE_ENABLE})
     file(GLOB CUR_SOURCES "${SUB_DIR}/*.cpp")
-    set(CUR_INCLUDES ${LIB_DIR}/protocol ${SUB_DIR})
+    set(CUR_INCLUDES ${LIB_DIR}/host-protocol ${SUB_DIR})
 
     SUB_ADD_SRC(CUR_SOURCES)
     SUB_ADD_INC(CUR_INCLUDES)
