0fafed16356ee7d9dd573b84f7267d05daf35e1f
xiao <2592509183@qq.com>
2023-05-02 19:51:15 +0800

    添加TopicShareClientUart

--- src/module/topic_share_uart/mod_topic_share_uart.hpp
+++ src/module/topic_share_uart/mod_topic_share_uart.hpp
@@ -33,4 +33,41 @@ class TopicShareServerUart {
   Message::Topic<Data> topic_;
   typename Message::Topic<Data>::RemoteData data_;
 };
+
+class TopicShareClientUart {
+ public:
+  typedef struct {
+    std::vector<const char*> topic_name;
+    bool block;
+    bsp_uart_t uart;
+    uint32_t cycle;
+  } Param;
+
+  TopicShareClientUart(Param& param) : param_(param), remote_(128, 10) {
+    for (auto name : param_.topic_name) {
+      remote_.AddTopic(name);
+    }
+
+    auto thread_fn = [](TopicShareClientUart* share) {
+      while (true) {
+        bsp_uart_receive(share->param_.uart, &share->recv_buff[0],
+                         sizeof(share->recv_buff), share->param_.block);
+        share->remote_.PraseData(&share->recv_buff[0],
+                                 bsp_uart_get_count(share->param_.uart));
+        share->thread_.Sleep(share->param_.cycle);
+      }
+    };
+
+    thread_.Create(thread_fn, this, "topic_share_client", 1024,
+                   System::Thread::HIGH);
+  };
+
+  Param param_;
+
+  Message::Remote remote_;
+
+  std::array<uint8_t, 128> recv_buff;
+
+  System::Thread thread_;
+};
 }  // namespace Module
