787df0175fca6fc184b33f91b14d8bb0fae4b4a3
xiao <2592509183@qq.com>
2023-05-02 15:35:48 +0800

    添加net_info topic

--- src/module/ble_net_config/mod_ble_net_config.hpp
+++ src/module/ble_net_config/mod_ble_net_config.hpp
@@ -3,16 +3,24 @@
 #include "bsp_ble_server.h"
 #include "bsp_wifi_client.h"
 #include "module.hpp"
+#include "om.hpp"
 #include "om_log.h"
 
 namespace Module {
 class BleNetConfig {
  public:
-  BleNetConfig() {
+  typedef struct {
+    uint8_t net_name[30];
+    uint8_t net_password[30];
+    bool connected;
+  } Data;
+
+  BleNetConfig() : net_info_("net_info") {
     bsp_ble_server_init("XRobot net config");
     bsp_wifi_client_init();
     auto thread_fn = [](BleNetConfig* ble) {
       while (true) {
+        ble->net_info_.Publish(ble->data_);
         uint32_t count = bsp_ble_server_avaliable();
         if (!count) {
           ble->thread_.Sleep(100);
@@ -23,7 +31,7 @@ class BleNetConfig {
 
         int ans = sscanf(reinterpret_cast<const char*>(ble->recv_buff),
                          "xrobot net_name %s net_password %s end",
-                         ble->net_name, ble->net_password);
+                         ble->data_.net_name, ble->data_.net_password);
 
         if (ans == EOF) {
           continue;
@@ -31,8 +39,8 @@ class BleNetConfig {
 
         (void)snprintf(reinterpret_cast<char*>(ble->recv_buff),
                        sizeof(ble->recv_buff),
-                       "BLE get net name:%s, net password:%s", ble->net_name,
-                       ble->net_password);
+                       "BLE get net name:%s, net password:%s",
+                       ble->data_.net_name, ble->data_.net_password);
 
         bsp_ble_server_transmit(ble->recv_buff,
                                 strnlen(reinterpret_cast<char*>(ble->recv_buff),
@@ -40,8 +48,8 @@ class BleNetConfig {
 
         OMLOG_PASS("%s", ble->recv_buff);
 
-        bsp_wifi_connect(reinterpret_cast<char*>(ble->net_name),
-                         reinterpret_cast<char*>(ble->net_password));
+        bsp_wifi_connect(reinterpret_cast<char*>(ble->data_.net_name),
+                         reinterpret_cast<char*>(ble->data_.net_password));
 
         uint32_t timeout = 0;
 
@@ -52,11 +60,13 @@ class BleNetConfig {
             OMLOG_NOTICE("Waiting for wifi connection, times %d",
                          timeout / 1000);
             if (timeout > 5000) {
-              OMLOG_ERROR("Fail connect to wifi: %s", ble->net_name);
+              OMLOG_ERROR("Fail connect to wifi: %s", ble->data_.net_name);
+              ble->data_.connected = false;
               break;
             }
           } else {
-            OMLOG_PASS("Success connect to wifi: %s", ble->net_name);
+            OMLOG_PASS("Success connect to wifi: %s", ble->data_.net_name);
+            ble->data_.connected = true;
             break;
           }
         }
@@ -67,8 +77,10 @@ class BleNetConfig {
   }
 
   uint8_t recv_buff[100];
-  uint8_t net_name[30];
-  uint8_t net_password[30];
+
+  Data data_;
+
+  Message::Topic<Data> net_info_;
 
   System::Thread thread_;
 };
