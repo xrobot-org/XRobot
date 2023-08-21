1fc37cc528471155d96f42a13d12e6ad0ff44ae9
xiao <2592509183@qq.com>
2023-08-22 00:21:03 +0800

    修复STM32平台系统时间获取概率出错

--- hw/bsp/c-mini/drivers/bsp_time.c
+++ hw/bsp/c-mini/drivers/bsp_time.c
@@ -4,13 +4,18 @@
 #include "main.h"
 #include "task.h"
 
-extern TIM_HandleTypeDef htim14;
-
 uint32_t bsp_time_get_ms() { return xTaskGetTickCount(); }
 
 uint64_t bsp_time_get_us() {
-  return xTaskGetTickCount() * 1000 + 1000 -
-         (SysTick->VAL * 1000 / SysTick->LOAD);
+  uint32_t ms_old = xTaskGetTickCount();
+  uint32_t tick_value_old = SysTick->VAL;
+  uint32_t ms_new = xTaskGetTickCount();
+  uint32_t tick_value_new = SysTick->VAL;
+  if (ms_old == ms_new) {
+    return ms_new * 1000 + 1000 - tick_value_old * 1000 / (SysTick->LOAD + 1);
+  } else {
+    return ms_new * 1000 + 1000 - tick_value_new * 1000 / (SysTick->LOAD + 1);
+  }
 }
 
 uint64_t bsp_time_get() __attribute__((alias("bsp_time_get_us")));
