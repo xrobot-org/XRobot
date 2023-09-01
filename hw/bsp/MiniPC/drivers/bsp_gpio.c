ea00f92c86a9b6bbe10475f3308d0d906a5996b4
xiao <2592509183@qq.com>
2023-09-01 14:28:48 +0800

    整理MiniPC gpio 代码

--- hw/bsp/MiniPC/drivers/bsp_gpio.c
+++ hw/bsp/MiniPC/drivers/bsp_gpio.c
@@ -2,14 +2,11 @@
 
 #include <stdlib.h>
 
+#include "bsp_def.h"
+
 inline bsp_status_t bsp_gpio_write_pin(bsp_gpio_t gpio, bool value) {
-  (void)gpio;
+  XB_UNUSED(gpio);
+  XB_UNUSED(value);
 
-  if (value) {
-    // if (!system("spd-say -r 100 -p 100 -R 100  滴")) {
-    //   return BSP_OK;
-    // }
-  }
-
-  return BSP_ERR;
+  return BSP_OK;
 }
