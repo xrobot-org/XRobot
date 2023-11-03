4924df466069b01c84d43979d0e2185ebd5ebebf
xiao <2592509183@qq.com>
2023-11-03 20:43:21 +0800

    canfd_imu支持有无Bl两个版本

--- hw/bsp/atom/board.cmake
+++ hw/bsp/atom/board.cmake
@@ -13,8 +13,16 @@ add_subdirectory(${BOARD_DIR}/drivers)
 
 add_executable(${PROJECT_NAME}.elf ${BOARD_DIR}/main.cpp)
 
+add_executable(${PROJECT_NAME}_with_bl.elf ${BOARD_DIR}/main.cpp)
+
 target_link_libraries(
   ${PROJECT_NAME}.elf
+  PUBLIC bsp
+  PUBLIC system
+  PUBLIC robot)
+
+target_link_libraries(
+  ${PROJECT_NAME}_with_bl.elf
   PUBLIC bsp
   PUBLIC system
   PUBLIC robot)
@@ -28,6 +36,20 @@ target_include_directories(
   PRIVATE $<TARGET_PROPERTY:robot,INTERFACE_INCLUDE_DIRECTORIES>
 )
 
+target_include_directories(
+  ${PROJECT_NAME}_with_bl.elf
+  PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}
+  PRIVATE $<TARGET_PROPERTY:bsp,INTERFACE_INCLUDE_DIRECTORIES>
+  PRIVATE $<TARGET_PROPERTY:system,INTERFACE_INCLUDE_DIRECTORIES>
+  PRIVATE $<TARGET_PROPERTY:robot,INTERFACE_INCLUDE_DIRECTORIES>
+)
+
+target_link_options(${PROJECT_NAME}_with_bl.elf PRIVATE "-T${BOARD_DIR}/ld/LinkerScripts_with_bl.ld")
+
 create_hex_output(${PROJECT_NAME})
 create_bin_output(${PROJECT_NAME})
 print_section_sizes(${PROJECT_NAME}.elf)
+
+create_hex_output(${PROJECT_NAME}_with_bl)
+create_bin_output(${PROJECT_NAME}_with_bl)
+print_section_sizes(${PROJECT_NAME}_with_bl.elf)
