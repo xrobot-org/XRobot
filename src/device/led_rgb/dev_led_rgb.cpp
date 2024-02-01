d78e24dea20319f6ecdf2028ba3e0910b193f986
刘骢 <2592509183@qq.com>
2024-02-01 12:46:46 +0800

    rgb闪烁通过改变占空比而不是关闭pwm实现

--- src/device/led_rgb/dev_led_rgb.cpp
+++ src/device/led_rgb/dev_led_rgb.cpp
@@ -6,7 +6,7 @@ using namespace Device;
 
 static uint32_t led_stats;
 
-RGB::RGB() {
+RGB::RGB(bool auto_start) {
   bsp_pwm_start(BSP_PWM_LED_RED);
   bsp_pwm_start(BSP_PWM_LED_BLU);
   bsp_pwm_start(BSP_PWM_LED_GRN);
@@ -19,21 +19,21 @@ RGB::RGB() {
     while (1) {
       switch (led_fsm) {
         case 0:
-          led->Set(GREEN, ON, 1);
-          led->Set(RED, OFF, 1);
-          led->Set(BLUE, OFF, 1);
+          led->Set(GREEN, 1);
+          led->Set(RED, 0);
+          led->Set(BLUE, 0);
           led_fsm++;
           break;
         case 1:
-          led->Set(GREEN, OFF, 1);
-          led->Set(RED, ON, 1);
-          led->Set(BLUE, OFF, 1);
+          led->Set(GREEN, 0);
+          led->Set(RED, 1);
+          led->Set(BLUE, 0);
           led_fsm++;
           break;
         case 2:
-          led->Set(GREEN, OFF, 1);
-          led->Set(RED, OFF, 1);
-          led->Set(BLUE, ON, 1);
+          led->Set(GREEN, 0);
+          led->Set(RED, 0);
+          led->Set(BLUE, 1);
           led_fsm = 0;
           break;
       }
@@ -42,11 +42,13 @@ RGB::RGB() {
     }
   };
 
-  this->thread_.Create(led_thread, this, "led_thread",
-                       DEVICE_LED_RGB_TASK_STACK_DEPTH, System::Thread::LOW);
+  if (auto_start) {
+    this->thread_.Create(led_thread, this, "led_thread",
+                         DEVICE_LED_RGB_TASK_STACK_DEPTH, System::Thread::LOW);
+  }
 }
 
-bool RGB::Set(RGB::Channel ch, RGB::Status status, float duty_cycle) {
+bool RGB::Set(RGB::Channel ch, float duty_cycle) {
   clampf(&duty_cycle, 0.0f, 1.0f);
 
   bsp_pwm_channel_t pwm_ch = BSP_PWM_NUMBER;
@@ -68,28 +70,6 @@ bool RGB::Set(RGB::Channel ch, RGB::Status status, float duty_cycle) {
       break;
     default:
       pwm_ch = BSP_PWM_LED_RED;
-  }
-
-  switch (status) {
-    case ON:
-      bsp_pwm_start(pwm_ch);
-      led_stats |= pwm_ch;
-      break;
-
-    case OFF:
-      bsp_pwm_stop(pwm_ch);
-      led_stats &= ~pwm_ch;
-      break;
-
-    case TAGGLE:
-      if (led_stats & pwm_ch) {
-        bsp_pwm_stop(pwm_ch);
-        led_stats &= ~pwm_ch;
-      } else {
-        bsp_pwm_start(pwm_ch);
-        led_stats |= pwm_ch;
-      }
-      break;
   }
 
   return true;
