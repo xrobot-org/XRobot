937d0c4ad9c0a28b8d9a46af35550482750bfe70
52cwc <1340561738@qq.com>
2024-05-19 12:44:03 +0800

    修改自定义控制器

--- src/device/custom_controller/dev_custom_controller.hpp
+++ src/device/custom_controller/dev_custom_controller.hpp
@@ -1,12 +1,37 @@
+#include "comp_cmd.hpp"
 #include "comp_ui.hpp"
 #include "device.hpp"
 
 namespace Device {
 class CustomController {
  public:
-  CustomController();
+  typedef struct {
+    float offset[6];
+  } Param;
+
+  CustomController(Param& param);
+
+  typedef struct __attribute__((packed)) {
+    struct __attribute__((packed)) {
+      uint8_t sof;           // 起始字节，固定值为0xA5
+      uint16_t data_length;  // 数据帧中data的长度
+      uint8_t seq;           // 包序号
+      uint8_t crc8;          // 帧头CRC8校验
+    } frame_header;          // 帧头
+    uint16_t cmd_id;         // 命令码
+    union __attribute__((packed)) {
+      float angle[6];  // 自定义控制器的数据帧
+      uint8_t raw_data[30];
+    };
+  } Data;  // 自定义控制器数据包
 
   typedef enum { NUM = 144 } ControllerEvent;
+
+  Param param_;
+
+  bool online_ = false;
+
+  uint32_t last_online_time_ = 0;
 
   bool StartRecv();
 
@@ -14,15 +39,12 @@ class CustomController {
 
   void Offline();
 
- private:
-  System::Semaphore packet_recv_ = System::Semaphore(false);
+  Data data_;
 
-  Message::Topic<Component::CMD::Data> controller_angel_ =
-      Message::Topic<Component::CMD::Data>("collectangle");
+  System::Semaphore packet_recv_ = System::Semaphore(false);
 
   Message::Event event_;
   System::Thread recv_thread_;
   System::Thread trans_thread_;
-  Component::CMD::Data controller_data_{};
 };
 }  // namespace Device
--- src/module/custom_controller/mod_custom_controller.hpp
+++ src/module/custom_controller/mod_custom_controller.hpp
@@ -1,8 +1,31 @@
 #include "bsp_uart.h"
+#include "comp_crc16.hpp"
+#include "comp_crc8.hpp"
 #include "dev_mt6701.hpp"
 #include "module.hpp"
 
-static char uart_tx_buff[1024] = {0};
+#define FRAME_HEADER_LENGTH 5  // 帧头数据长度
+#define CMD_ID_LENGTH 2        // 命令码ID数据长度
+#define DATA_LENGTH 30         // 数据段长度
+#define FRAME_TAIL_LENGTH 2    // 帧尾数据长度
+
+#define DATA_FRAME_LENGTH                              \
+  (FRAME_HEADER_LENGTH + CMD_ID_LENGTH + DATA_LENGTH + \
+   FRAME_TAIL_LENGTH)  // 整个数据帧的长度
+
+#define CONTROLLER_CMD_ID 0x0302  // 自定义控制器命令码
+
+typedef struct __attribute__((packed)) {
+  struct __attribute__((packed)) {
+    uint8_t sof;                 // 起始字节，固定值为0xA5
+    uint16_t data_length;        // 数据帧中data的长度
+    uint8_t seq;                 // 包序号
+    uint8_t crc8;                // 帧头CRC8校验
+  } frame_header;                // 帧头
+  __packed uint16_t cmd_id;      // 命令码
+  __packed uint8_t data[30];     // 自定义控制器的数据帧
+  __packed uint16_t frame_tail;  // 帧尾CRC16校验
+} Controller;                    // 自定义控制器数据包
 
 namespace Module {
 class CustomController {
@@ -10,6 +33,7 @@ class CustomController {
   typedef struct {
     Device::MT6701::Param mt6701[6];
   } Param;
+
   CustomController(Param& param)
       : mt6701_1(param.mt6701[0]),
         mt6701_2(param.mt6701[1]),
@@ -18,18 +42,22 @@ class CustomController {
         mt6701_5(param.mt6701[4]),
         mt6701_6(param.mt6701[5]) {
     auto task_fun = [](CustomController* ctrl) {
-      (void)snprintf(
-          uart_tx_buff, sizeof(uart_tx_buff), "%f,%f,%f,%f,%f,%f\n",
-          ctrl->mt6701_1.angle_.Value(), ctrl->mt6701_2.angle_.Value(),
-          ctrl->mt6701_3.angle_.Value(), ctrl->mt6701_4.angle_.Value(),
-          ctrl->mt6701_5.angle_.Value(), ctrl->mt6701_6.angle_.Value());
-
-      bsp_uart_transmit(BSP_UART_MCU, reinterpret_cast<uint8_t*>(uart_tx_buff),
-                        strlen(uart_tx_buff), false);
+      ctrl->txbuff[0] = ctrl->mt6701_1.angle_.Value();
+      ctrl->txbuff[1] = ctrl->mt6701_2.angle_.Value();
+      ctrl->txbuff[2] = ctrl->mt6701_3.angle_.Value();
+      ctrl->txbuff[3] = ctrl->mt6701_4.angle_.Value();
+      ctrl->txbuff[4] = ctrl->mt6701_5.angle_.Value();
+      ctrl->txbuff[5] = ctrl->mt6701_6.angle_.Value();
+      ctrl->DataConcatenation(reinterpret_cast<uint8_t*>(ctrl->txbuff),
+                              sizeof(txbuff));
+      bsp_uart_transmit(BSP_UART_MCU, reinterpret_cast<uint8_t*>(&ctrl->txdata),
+                        DATA_FRAME_LENGTH, false);
     };
 
-    System::Timer::Create(task_fun, this, 10);
+    System::Timer::Create(task_fun, this, 40);
   }
+
+  void DataConcatenation(uint8_t* data, uint16_t data_lenth);
 
   Device::MT6701 mt6701_1;
   Device::MT6701 mt6701_2;
@@ -37,5 +65,8 @@ class CustomController {
   Device::MT6701 mt6701_4;
   Device::MT6701 mt6701_5;
   Device::MT6701 mt6701_6;
+
+  float txbuff[6] = {0};
+  Controller txdata;
 };
 }  // namespace Module
