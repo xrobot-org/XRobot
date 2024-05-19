743ed6021a6026386486f7bba70a6e56f2be2821
张皓琳 <18981222025@163,com>
2024-05-19 21:29:06 +0800

    提交飞镖相关代码

--- src/module/dart_launcher/mod_dart_launcher.hpp
+++ src/module/dart_launcher/mod_dart_launcher.hpp
@@ -12,8 +12,6 @@ class DartLauncher {
   typedef struct {
     Device::AutoCaliLimitedMech<Device::RMMotor, Device::RMMotor::Param,
                                 1>::Param rod_actr;
-    Device::AutoCaliLimitedMech<Device::RMMotor, Device::RMMotor::Param,
-                                1>::Param reload_actr;
     const std::vector<Component::CMD::EventMapItem> EVENT_MAP;
 
     std::array<Component::SpeedActuator::Param, 4> fric_actr;
@@ -21,26 +19,44 @@ class DartLauncher {
   } Param;
 
   typedef struct {
-    float rod;
-    float reload;
+    float rod_position;
     float fric_speed;
   } Setpoint;
-
   typedef enum {
-    RELOAD,
-    RESET,
-    ON,
-    OFF,
-  } Event;
+    FRIC_OUTOST, /*摩擦轮前哨战模式*/
+    FRIC_BASE,   /*摩擦轮基地模式*/
+    FRIC_OFF,
+  } FricMode;
+  typedef enum {
+    ADVANCE, /* 推进1个单位 */
+    STAY,
+    BACK, /* 回到初始位置0,1 */
+  } RodMode;
+  typedef enum {
+    RELAX, /* 摩擦轮不输出，回到初始位置0.1 */
+    SET_FRIC_OFF,
+    SET_STAY,
+    RESET_POSITION,  /* 回到初始位置0.1 */
+    FIRE,            /* 发射 */
+    SET_FRIC_OUTOST, /*攻击前哨站*/
+    SET_FRIC_BASE,   /*攻击基地*/
+  } DartEvent;       /* 事件 */
 
   DartLauncher(Param& param, float control_freq);
 
   void UpdateFeedback();
 
   void Control();
+  void SetRodMode(RodMode mode);
+  void SetFricMode(FricMode mode);
 
  private:
+  System::Semaphore ctrl_lock_;
+
   Param& param_;
+
+  FricMode fric_mode_;
+  RodMode rod_mode_;
 
   Setpoint setpoint_;
 
@@ -49,14 +65,10 @@ class DartLauncher {
   uint64_t last_wakeup_ = 0;
 
   uint64_t now_ = 0;
-
-  uint32_t last_reload_time_ = 0;
+  int rod_position_ = 0; /* 丝杆位置0,1,2,3循环 */
 
   Device::AutoCaliLimitedMech<Device::RMMotor, Device::RMMotor::Param, 1>
-      rod_actr_;
-  Device::AutoCaliLimitedMech<Device::RMMotor, Device::RMMotor::Param, 1>
-      reload_actr_;
-
+      rod_actr_; /* 内含电机、执行器、防堵数据 */
   std::array<Component::SpeedActuator*, 4> fric_actr_;
   std::array<Device::RMMotor*, 4> fric_motor_;
 
