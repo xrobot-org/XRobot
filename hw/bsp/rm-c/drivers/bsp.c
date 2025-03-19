eafec4dbc09f7317b29807d63c1f9363ed66543a
Xiao <2592509183@qq.com>
2025-03-19 20:10:09 +0000

    Fix rm-c bsp_uart_init position.

--- hw/bsp/rm-c/drivers/bsp.c
+++ hw/bsp/rm-c/drivers/bsp.c
@@ -19,8 +19,6 @@ void bsp_init() {
   SystemClock_Config();
 
   /* Initialize all configured peripherals */
-  bsp_uart_init();
-
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
@@ -47,4 +45,5 @@ void bsp_init() {
   MX_IWDG_Init();
 #endif
   HAL_TIM_Base_Stop_IT(&htim6);
+  bsp_uart_init();
 }
